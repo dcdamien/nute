8bb826d8ad893885506b8a9d5f920a2f4e14fce4
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2010-04-09 07:49:25 +0000

    ADC-based random generator added. Doesn't work.

---
+++ Foxlocket/Firmware/random.h
@@ -0,0 +1,28 @@
+/* 
+ * File:   random.h
+ * Author: Kreyl Laurelindo
+ *
+ * Created on 9 Апрель 2010 г., 10:18
+ */
+
+#ifndef _RANDOM_H
+#define	_RANDOM_H
+
+#include <inttypes.h>
+#include <stdbool.h>
+
+
+#define RANDOM_LENGTH   3 // bits
+
+struct Random_t{
+    uint8_t Random;
+    bool IsReady;
+    uint8_t counter;
+};
+
+extern struct Random_t ERandom;
+
+void RandomStart(void);
+
+#endif	/* _RANDOM_H */
+
