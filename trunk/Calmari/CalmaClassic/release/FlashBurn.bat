9bad4a2aedc0af43b65510eaf0698942f57a3834
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2010-06-18 21:35:33 +0000

    directories moved

---
+++ trunk/Calmari/CalmaClassic/release/FlashBurn.bat
@@ -0,0 +1,5 @@
+@rem avrdude.exe -p attiny45 -P com1 -c ponyser -U flash:w:MainUnit.hex
+
+@rem D:\Electronix\AVRTools\USBProg\AVROSP.exe -cCOM2 -dATtiny45 -pf -vf -ifD:\Electronix\progs\=Calmari=\CalmaClassic\MainUnit.hex
+"d:\Electronix\AVRTools\AVR910 USBProg\AVROSP.exe" -cCOM4 -dATtiny45 -pf -vf -ifMainUnit.hex -e
+
