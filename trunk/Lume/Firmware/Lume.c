8d26dba195124661b4d06f4411b6e8b4dd805542
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2010-08-19 20:10:04 +0000

    Lume: firmware splitted to several files.

--- trunk/Lume/Firmware/Lume.c
+++ trunk/Lume/Firmware/Lume.c
@@ -1,5 +1,6 @@
 #include "Lume.h"
 #include "common.h"
+#include "led_io.h"
 #include <util/delay.h>
 #include <util/atomic.h>
 
@@ -11,57 +12,6 @@ struct {
 } Time;
 
 enum {ModeRegular, ModeSetHours, ModeSetMinutes} Mode;
-
-struct {
-    uint8_t HByte, MByte;   // Control bytes
-    // PWMs
-    uint8_t PWM_Top;
-    struct PWM_t Min0PWM, Min1PWM, Hr0PWM, Hr1PWM;
-} LControl;
-
-volatile uint16_t TickCounter;
-
-// Tables of accordance
-const uint8_t HTable[12] = {
-    0b0, // H0 == H12
-    0b0, // H1
-    0b0, // H2
-    0b0, // H3
-    0b0, // H4
-    0b0, // H5
-    0b0, // H6
-    0b0, // H7
-    0b0, // H8
-    0b0, // H9
-    0b0, // H10
-    0b0, // H11
-};
-const uint8_t MTable[24][2] = { // First byte for minutes bits, second for minute byte
-    {0b10000000, 0b00101000},   // M12
-    {0b00000000, 0b01000000},   // M0.5
-    {0b00000000, 0b10000000},   // M1
-    {0b00000000, 0b01000001},   // M1.5
-    {0b00000000, 0b10001000},   // M2
-    {0b00000000, 0b01000010},   // M2.5
-    {0b00000000, 0b10010000},   // M3
-    {0b00000000, 0b01000011},   // M3.5
-    {0b00000000, 0b10011000},   // M4
-    {0b00000000, 0b01000100},   // M4.5
-    {0b00000000, 0b10100000},   // M5
-    {0b00000000, 0b01000101},   // M5.5
-    {0b00000000, 0b10101000},   // M6
-    {0b01000000, 0b00000000},   // M6.5
-    {0b10000000, 0b00000000},   // M7
-    {0b01000000, 0b00000001},   // M7.5
-    {0b10000000, 0b00001000},   // M8
-    {0b01000000, 0b00000010},   // M8.5
-    {0b10000000, 0b00010000},   // M9
-    {0b01000000, 0b00000011},   // M9.5
-    {0b10000000, 0b00011000},   // M10
-    {0b01000000, 0b00000100},   // M10.5
-    {0b10000000, 0b00100000},   // M11
-    {0b01000000, 0b00000101},   // M11.5
-};
 
 // ============================ Implementation =================================
 int main (void) {
@@ -76,28 +26,9 @@ int main (void) {
 
 FORCE_INLINE void GeneralInit(void) {
     // Light control
-    L_DDR  |=   (1<<DATA_IN)|(1<<LATCH)|(1<<SRCLK);
-    L_PORT &= ~((1<<DATA_IN)|(1<<LATCH)|(1<<SRCLK));
-    M_PWM_DDR  |= (1<<M0PWM)|(1<<M1PWM);
-    H_PWM_DDR  |= (1<<H0PWM)|(1<<H1PWM);
-    M_PWM_PORT |= (1<<M0PWM)|(1<<M1PWM);
-    H_PWM_PORT |= (1<<H0PWM)|(1<<H1PWM);
-    // Hours PWM
-    TCCR1A = (0<<WGM11)|(1<<WGM10);
-    TCCR1B = (0<<WGM13)|(1<<WGM12)|(1<<CS12)|(0<<CS11)|(0<<CS10);
-    // Minutes PWM & delay counter
-    TCCR0A = (1<<WGM01)|(1<<WGM00);
-    TCCR0B = (0<<WGM02)|(0<<CS02)|(0<<CS01)|(1<<CS00);
-    TIMSK0 = (1<<TOIE0);
-
-    // Serial port setup
-    UCSR0B = (1<<TXEN0);    // TX enable, irqs disable
-    UCSR0C = (1<<UMSEL01)|(1<<UMSEL00)|(0<<UDORD0)|(0<<UCPHA0)|(0<<UCPOL0); // MSB first, leading edge, idle low
-    UBRR0  = 0; // Max speed
+    LedIOInit();
 
     // Keys
-
-
     // Timer2: realtime clock counter
     //TCCR2B = (0<<CS22)|(0<<CS21)|(1<<CS20);	// DEBUG: no division
     TCCR2B = (0<<CS22)|(1<<CS21)|(0<<CS20);	// DEBUG: div 8
@@ -105,18 +36,12 @@ FORCE_INLINE void GeneralInit(void) {
     ASSR  = (1<<AS2);				// Set Async mode of the timer
     TCCR2A = 0;
     //TCCR2B = (1<<CS22)|(0<<CS21)|(1<<CS20);	// RELEASE: div 128, once per second
-    TIMSK2 |= (1<<TOIE2);
+    TIMSK2 |= (1<<TOIE2);                       // Interrupt is in delay_util
 
     // Setup initial values
     Time.Second = 1;
     Time.HyperMinute = 1;
     Time.Hour = 0;
-
-    LControl.PWM_Top = 255;
-    LControl.Min0PWM.OCRX = &OCR0A;
-    LControl.Min1PWM.OCRX = &OCR0B;
-    LControl.Hr0PWM.OCRX  = &OCR1AL;
-    LControl.Hr1PWM.OCRX  = &OCR1BL;
 
     Mode = ModeRegular;
 
@@ -137,42 +62,6 @@ void TASK_Toggle(void) {
 
     TogglePWM(&LControl.Min0PWM);
     TogglePWM(&LControl.Min1PWM);
-}
-
-// =============================== Light control ===============================
-FORCE_INLINE void WriteControlBytes(void) {
-    while (bit_is_clear(UCSR0A, UDRE0));    // Wait until buffer is empty
-    UDR0 = LControl.MByte;
-    while (bit_is_clear(UCSR0A, UDRE0));    // Wait until buffer is empty
-    UDR0 = LControl.HByte;
-    // Toggle latch
-    UCSR0A = (1<<TXC0);                     // Reset TXC0 flag
-    while (bit_is_clear(UCSR0A, TXC0));     // Wait until shift register is empty
-    L_PORT |=  (1<<LATCH);
-    nop();
-    L_PORT &= ~(1<<LATCH);
-}
-
-void TogglePWM(struct PWM_t *pwm) {
-    if(pwm->Mode == PWMHold) return;                            // Nothing to do here
-    if(!TimerDelayElapsed(&(pwm->Timer), pwm->Delay)) return;   // Not in time
-    if(pwm->Mode == PWMRise) {
-        pwm->Value++;
-        if(pwm->Value == LControl.PWM_Top) pwm->Mode = PWMHold;
-
-    } // if mode
-    else {
-        pwm->Value--;
-        if(pwm->Value == 0) {
-            pwm->Mode = PWMHold;
-            // Disconnect PWM
-            if     (pwm->OCRX == &OCR0A)  TCCR0A &= ~((1<<COM0A1)|(1<<COM0A0));
-            else if(pwm->OCRX == &OCR0B)  TCCR0A &= ~((1<<COM0B1)|(1<<COM0B0));
-            else if(pwm->OCRX == &OCR1AL) TCCR1A &= ~((1<<COM1A1)|(1<<COM1A0));
-            else if(pwm->OCRX == &OCR1BL) TCCR1A &= ~((1<<COM1B1)|(1<<COM1B0));
-        }
-    }
-    *(pwm->OCRX) = pwm->Value;
 }
 
 // ============================ Events =========================================
@@ -217,58 +106,6 @@ void EVENT_NewHyperMinute(void) {
     WriteControlBytes();
 }
 
-void SetupMinute(uint8_t AMinute, enum PWMMode_t AMode) {
-    // Setup channel
-    LControl.HByte |= MTable[AMinute][0];
-    LControl.MByte |= MTable[AMinute][1];
-    // Setup PWM
-    if(AMinute & 0x01) SetupPWM(&LControl.Min0PWM, AMode);    // even => integers: 1 means M0_5, 2 - M1 and so on
-    else               SetupPWM(&LControl.Min1PWM, AMode);
-}
-
-void SetupPWM(struct PWM_t *pwm, enum PWMMode_t mode) {
-    pwm->Mode = mode;
-    TimerResetDelay(&(pwm->Timer));
-    if(mode == PWMRise) {
-        pwm->Value = 0;
-        *(pwm->OCRX) = 0;
-        pwm->Delay = PWMDelay1;
-    }
-    else {
-        pwm->Value = LControl.PWM_Top;
-        *(pwm->OCRX) = LControl.PWM_Top;
-        pwm->Delay = PWMDelay1;     // !!!!!!!FIXME
-    } // else
-
-    // Switch PWM on
-    if     (pwm->OCRX == &OCR0A)  TCCR0A |= ((1<<COM0A1)|(1<<COM0A0));
-    else if(pwm->OCRX == &OCR0B)  TCCR0A |= ((1<<COM0B1)|(1<<COM0B0));
-    else if(pwm->OCRX == &OCR1AL) TCCR1A |= ((1<<COM1A1)|(1<<COM1A0));
-    else if(pwm->OCRX == &OCR1BL) TCCR1A |= ((1<<COM1B1)|(1<<COM1B0));
-}
-
-
-// ============================ Delay utils ====================================
-bool TimerDelayElapsed(uint16_t *AVar, const uint16_t ADelay) {
-    uint16_t ttmp;
-    bool Result = false;
-    ATOMIC_BLOCK (ATOMIC_RESTORESTATE) {
-        ttmp = TickCounter;
-        if ((ttmp - *AVar) >= ADelay) {
-            *AVar = ttmp;   // Reset delay
-            Result = true;
-        }
-    } // Atomic
-    return Result;
-}
-
-void TimerResetDelay(uint16_t *AVar) {
-    ATOMIC_BLOCK (ATOMIC_RESTORESTATE) {
-        *AVar = TickCounter;
-    }
-}
-
-
 // =========================== Interrupts ======================================
 // Time counter
 ISR(TIMER2_OVF_vect) { 
@@ -289,9 +126,4 @@ ISR(TIMER2_OVF_vect) {
         } // if Hyperminute > 23
         if (POWER_OK()) EVENT_NewHyperMinute();
     }// Second
-}
-
-// Delay counter
-ISR(TIMER0_OVF_vect) {
-    TickCounter++;
 }
--- trunk/Lume/Firmware/Lume.h
+++ trunk/Lume/Firmware/Lume.h
@@ -1,3 +1,6 @@
+#ifndef _LUME_H
+#define	_LUME_H
+
 // ============================ Includes =======================================
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -13,30 +16,6 @@
 #define PWROK_P     PC3
 #define POWER_OK()  bit_is_set (PWROK_PIN, PWROK_P)
 
-// Lights
-#define L_DDR       DDRD
-#define L_PORT      PORTD
-#define DATA_IN     PD1
-#define LATCH       PD3
-#define SRCLK       PD4
-
-#define M_PWM_DDR   DDRD
-#define M_PWM_PORT  PORTD
-#define M0PWM       PD6
-#define M1PWM       PD5
-#define H_PWM_DDR   DDRB
-#define H_PWM_PORT  PORTB
-#define H0PWM       PB1
-#define H1PWM       PB2
-
-// ================================ Types ======================================
-enum PWMMode_t {PWMHold, PWMRise, PWMFade};
-struct PWM_t {
-    uint8_t Value;
-    uint16_t Timer, Delay;
-    enum PWMMode_t Mode;
-    volatile uint8_t *OCRX;
-};
 
 // ========================== Functions prototypes =============================
 void GeneralInit (void);
@@ -48,54 +27,4 @@ void TASK_Toggle(void);
 void EVENT_NewHour(void);
 void EVENT_NewHyperMinute(void);
 
-// Light control
-void SetupMinute(uint8_t AMinute, enum PWMMode_t AMode);
-
-void WriteControlBytes(void);
-void SetupPWM(struct PWM_t *pwm, enum PWMMode_t mode);
-void TogglePWM(struct PWM_t *pwm);
-
-// Time utils
-bool TimerDelayElapsed(uint16_t *AVar, const uint16_t ADelay);
-void TimerResetDelay(uint16_t *AVar);
-
-// ================================= Constants =================================
-// Timings
-#define PWMDelay1   99	// Low brightness
-#define PWMDelay2   4	// Mid brightness
-#define PWMDelay3   1	// High brightness
-
-// Light constants
-#define PWMStepOver1	27	// Where to switch to quick PWM change mode
-#define PWMStepOver2	45
-
-#define MAX_PWM		250
-#define MIN_PWM		10
-#define PWM_STEP	40
-
-// Light control
-#define M12     0
-#define M0_5    1
-#define M1      2
-#define M1_5    3
-#define M2      4
-#define M2_5    5
-#define M3      6
-#define M3_5    7
-#define M4      8
-#define M4_5    9
-#define M5      10
-#define M5_5    11
-#define M6      12
-#define M6_5    13
-#define M7      14
-#define M7_5    15
-#define M8      16
-#define M8_5    17
-#define M9      18
-#define M9_5    19
-#define M10     20
-#define M10_5   21
-#define M11     22
-#define M11_5   23
-
+#endif
