35190650312dce18bfb5a4c2033e4eda76489eb8
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2010-12-01 13:52:30 +0000

    Deluvio: sensors are working correctly.

--- Deluvio/Firmware/main.c
+++ Deluvio/Firmware/main.c
@@ -10,32 +10,32 @@
 #include <avr/interrupt.h>
 #include "lcd110x.h"
 #include "sensors.h"
+#include "delay_util.h"
+#include "beep.h"
+#include "time.h"
 
 int main(void) {
     GeneralInit();
     
     LCD_PrintString_P(0, 2, PSTR("Deluvio"), false); 
+    //Beep(2);
 
     sei();
     
     while(1) {
         Task_Sensors();
-
-        if(SensorIsTouched(3)) 
-            LCD_BCKLT_ON();
-        else 
-            LCD_BCKLT_OFF();
+        Task_Beep();
     } // while(1)
 }
 
 FORCE_INLINE void GeneralInit(void) {
     QTouchInit();
-
+    DelayInit();
+    BeepInit();
+    LCD_Init();
+    TimeInit();
     // Pumps
     PUMP_DDR |= (1<<PUMP1P)|(1<<PUMP2P)|(1<<PUMP3P)|(1<<PUMP4P);
-    
-    LCD_Init();
-    
 }
 
 // Pumps
@@ -56,5 +56,39 @@ void PumpOff(enum pump_t APump) {
     }
 }
 
+// Time
+void PrintTime(uint8_t x, uint8_t y) {
+    LCD_GotoXYstr(x, y);
+    
+}
 
+// ============================= Events ========================================
+// Key events
+void EVENT_KeyDown(void) {
+    Beep(1);
+    LCD_GotoXY(0,0);
+    LCD_DrawChar('D', false);
+}
+void EVENT_KeyUp(void) {
+    Beep(1);
+    LCD_GotoXY(0,0);
+    LCD_DrawChar('U', false);
+}
+void EVENT_KeyMenu(void) {
+    Beep(1);
+    LCD_GotoXY(0,0);
+    LCD_DrawChar('M', false);
+}
+void EVENT_KeyAqua(void) {
+    Beep(1);
+    LCD_GotoXY(0,0);
+    LCD_DrawChar('A', false);
+}
 
+// Time events
+void EVENT_NewHour(void) {
+
+}
+void EVENT_NewMinute(void) {
+    
+}
--- Deluvio/Firmware/main.h
+++ Deluvio/Firmware/main.h
@@ -12,6 +12,13 @@
 #include <avr/io.h>
 #include <stdbool.h>
 
+// Power ok
+#define PWROK_DDR   DDRA
+#define PWROK_PORT  PORTA
+#define PWROK_PIN   PINA
+#define PWROK_P     PA3
+#define POWER_OK()  bit_is_set (PWROK_PIN, PWROK_P)
+
 // Pumps
 #define PUMP_DDR    DDRA
 #define PUMP_PORT   PORTA
@@ -21,11 +28,16 @@
 #define PUMP4P      PA7
 enum pump_t {Pump1, Pump2, Pump3, Pump4};
 
+// Modes
+//enum {ModeRegular, ModeSetHours, ModeSetMinutes} Mode;
+
 // ============================== Prototypes ===================================
+void GeneralInit(void);
+
 void PumpOn(enum pump_t APump);
 void PumpOff(enum pump_t APump);
 
-void GeneralInit(void);
+void PrintTime(uint8_t x, uint8_t y);
 
 
 #endif	/* MAIN_H */
