343f6705b19ed2b3df577633a22a09bf10c3efa3
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2011-03-01 19:29:15 +0000

    Failed IRQ-based CC1101 module

--- trunk/_Master_/ArmTemplate/cc1101_mdl04/main.cpp
+++ trunk/_Master_/ArmTemplate/cc1101_mdl04/main.cpp
@@ -18,20 +18,20 @@ void LEDInit(void) {
 
 int main(void) {
     GeneralInit();
-    Uart.PrintString("\rCC1101\r");
+    Uart.PrintString("\rCC1101 TX\r");
 
-    uint32_t LEDTmr;
+    //uint32_t LEDTmr;
     while (1) {
-        if (Delay.Elapsed(&LEDTmr, 250)) {
+      //  if (Delay.Elapsed(&LEDTmr, 250)) {
         //Delay.ms(200);
-            LED_TOGGLE();
+            //LED_TOGGLE();
             
-            uint8_t b = CC.ReadRegister(CC_IOCFG0);
-            Uart.PrintAsHex(b);
-            Uart.NewLine();
+//            uint8_t b = CC.ReadRegister(CC_IOCFG0);
+//            Uart.PrintAsHex(b);
+//            Uart.NewLine();
             //Uart.Print('a');
-        }
-        //Task_CC();
+        //}
+        Task_CC();
     }
 }
 
@@ -42,12 +42,15 @@ void GeneralInit(void) {
     Delay.Init();
     Uart.Init();
     CC.Init();
+    
 }
 
 // ============================ Events =========================================
 void EVENT_NewPacket(void) {
-
-
+    Uart.NewLine();
+    for (uint8_t i=0; i<PKT_FULL_LEN; i++)
+        Uart.PrintAsHex(CC.RX_PktArray[i]);
+    Uart.NewLine();
 }
 
 // ============================= IRQs ==========================================
