6efce809a78e0f87b1c47862af2fec5f882f7397
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2011-07-24 14:05:54 +0000

    QT library removed, self-made sensors work somehow.

--- Calmari/Candle02/Candle_fw/sensors.cpp
+++ Calmari/Candle02/Candle_fw/sensors.cpp
@@ -4,60 +4,19 @@
 #include <avr/wdt.h>
 
 #include "delay_util.h"
+#include "uart_soft.h"
 
 // ============================= Variables =====================================
-extern qt_touch_lib_config_data_t qt_config_data;
-extern qt_touch_lib_measure_data_t qt_measure_data; // touch output - measurement data
-
-/* current time, set by timer ISR */
-static volatile uint16_t current_time_ms_touch = 0u;
+Keys_t EKeys;
 
 
-#ifdef QTOUCH_STUDIO_MASKS
-extern TOUCH_DATA_T SNS_array[2][2];
-extern TOUCH_DATA_T SNSK_array[2][2];
-#endif
-
-struct Keys_t EKeys;
 
 // =========================== Implementation ==================================
 void Keys_t::Init(void) {
-    MCUCR |= (1<<PUD);  // Disable all pull-ups
-    #ifdef QTOUCH_STUDIO_MASKS
-    SNS_array[0][0]= 0x5;
-    SNS_array[0][1]= 0x0;
-    SNS_array[1][0]= 0x41;
-    SNS_array[1][1]= 0x0;
 
-    SNSK_array[0][0]= 0xA;
-    SNSK_array[0][1]= 0x0;
-    SNSK_array[1][0]= 0x82;
-    SNSK_array[1][1]= 0x0;
-    #endif
-
-    // Configure the Sensors as keys
-    qt_enable_key( CHANNEL_0, AKS_GROUP_1, 10u, HYST_6_25 );
-    qt_enable_key( CHANNEL_1, AKS_GROUP_1, 10u, HYST_6_25 );
-    qt_enable_key( CHANNEL_2, AKS_GROUP_1, 10u, HYST_6_25 );
-    qt_enable_key( CHANNEL_3, AKS_GROUP_1, 5u, HYST_6_25 );
-
-    //  Set the parameters like recalibration threshold, Max_On_Duration etc in this function by the user
-    /*  This will be modified by the user to different values   */
-    qt_config_data.qt_di              = DEF_QT_DI;
-    qt_config_data.qt_neg_drift_rate  = DEF_QT_NEG_DRIFT_RATE;
-    qt_config_data.qt_pos_drift_rate  = DEF_QT_POS_DRIFT_RATE;
-    qt_config_data.qt_max_on_duration = DEF_QT_MAX_ON_DURATION;
-    qt_config_data.qt_drift_hold_time = DEF_QT_DRIFT_HOLD_TIME;
-    qt_config_data.qt_recal_threshold = DEF_QT_RECAL_THRESHOLD;
-    qt_config_data.qt_pos_recal_delay = DEF_QT_POS_RECAL_DELAY;
-
-    /*  Address to pass address of user functions   */
-    /*  This function is called after the library has made capacitive measurements,
-    *   but before it has processed them. The user can use this hook to apply filter
-    *   functions to the measured signal values.(Possibly to fix sensor layout faults)    */
-    qt_filter_callback = 0;
-
-    qt_init_sensing();  // initialise touch sensing
+    DDRC &= ~(1<<PC3);
+    DDRC |=  (1<<PC2);
+    PORTC &= ~(1<<PC3);     // No pull-up
 
     // Init variables
     Key[0].EventPress = &EVENT_KeyDown;
@@ -69,41 +28,56 @@ void Keys_t::Init(void) {
 }
 
 void Keys_t::Task(void) {
-    if(!Delay.Elapsed(&Timer, QT_MEASUREMENT_PERIOD)) return;
-    // Update the current time
-    current_time_ms_touch += QT_MEASUREMENT_PERIOD;
+//    if(!Delay.Elapsed(&Timer, KEY_MEASUREMENT_PERIOD)) return;
+    if(!Delay.Elapsed(&Timer, 200)) return;
     // Measure
-    // Status flags to indicate the re-burst for library
-    uint16_t status_flag = 0u;
-    bool burst_flag = false;
-    do {
-        status_flag = qt_measure_sensors(current_time_ms_touch);
-        burst_flag  = status_flag & QTLIB_BURST_AGAIN;
-        wdt_reset();
-    } while (burst_flag);
+    uint16_t i;
+    ATOMIC_BLOCK(ATOMIC_FORCEON) {
+        DDRC  &= ~((1<<SNS)|(1<<SNSK));
+        PORTC &= ~((1<<SNS)|(1<<SNSK));
+        for(i=0; i<8192; i++) {
 
-    // Check if key event triggered
-    for (uint8_t i=0; i<4; i++) {
-        if (IsTouched(i) && !Key[i].IsDown) {
-            Key[i].IsDown = true;
-            if (Key[i].MayRepeat) ATOMIC_BLOCK(ATOMIC_FORCEON) {
-                Key[i].Delay = KEY_REPEAT_TIMEOUT;
-                Delay.Reset(&Key[i].Timer);
-            }
-            if (Key[i].EventPress != 0) Key[i].EventPress();
+            PORTC |=  (1<<SNSK);
+            nop();
+//            nop();
+//            nop();
+//            nop();
+            PORTC &= ~(1<<SNSK);
+            nop();
+
+            DDRC  |=  (1<<SNS);
+            nop();
+            DDRC  &= ~(1<<SNS);
+            //nop();
+            if (bit_is_set(PINC, SNSK)) break;
         }
-        else if (!IsTouched(i) && Key[i].IsDown) Key[i].IsDown = false;
+    }
+    UARTSendUint(i);
+    UARTNewLine();
 
-        // Check if continuous keypress
-        if (Key[i].MayRepeat) {
-            if (Key[i].IsDown) if (Delay.Elapsed(&Key[i].Timer, Key[i].Delay)) {
-                ATOMIC_BLOCK(ATOMIC_FORCEON) {
-                    Key[i].Delay = KEY_REPEAT_DELAY;
-                }
-                if (Key[i].EventPress != 0) Key[i].EventPress();
-            } // if is down && delay elapsed
-        } // if may repeat
-    } // for
+
+//    // Check if key event triggered
+//    for (uint8_t i=0; i<4; i++) {
+//        if (IsTouched(i) && !Key[i].IsDown) {
+//            Key[i].IsDown = true;
+//            if (Key[i].MayRepeat) ATOMIC_BLOCK(ATOMIC_FORCEON) {
+//                Key[i].Delay = KEY_REPEAT_TIMEOUT;
+//                Delay.Reset(&Key[i].Timer);
+//            }
+//            if (Key[i].EventPress != 0) Key[i].EventPress();
+//        }
+//        else if (!IsTouched(i) && Key[i].IsDown) Key[i].IsDown = false;
+//
+//        // Check if continuous keypress
+//        if (Key[i].MayRepeat) {
+//            if (Key[i].IsDown) if (Delay.Elapsed(&Key[i].Timer, Key[i].Delay)) {
+//                ATOMIC_BLOCK(ATOMIC_FORCEON) {
+//                    Key[i].Delay = KEY_REPEAT_DELAY;
+//                }
+//                if (Key[i].EventPress != 0) Key[i].EventPress();
+//            } // if is down && delay elapsed
+//        } // if may repeat
+//    } // for
 }
 
 
--- Calmari/Candle02/Candle_fw/sensors.h
+++ Calmari/Candle02/Candle_fw/sensors.h
@@ -10,12 +10,33 @@
 
 #include <stdbool.h>
 #include <inttypes.h>
-#include "touch_qt_config.h"
-#include "touch_api.h"
+
+/*
+ * 1) SNS and SNSK both low to discharge Cs.
+ * 2) SNS tri-state.
+ * 3) SNSK high output to charge the cap formed by electrode/dielectric/finger.
+ * 4) SNSK tri-state
+ * 5) SNS low output - cap [e/d/f] transfers charge to Cs
+ * 6) Check voltage on SNSK to see if it's reached threshold yet. If so, go to Step 9.
+ * 7) SNS tri-state.
+ * 8) Increment a counter and go to step 3.
+ */
 
 // ================================= Defines ===================================
+#define SNS             PC0
+#define SNSK            PC1
+#define BOTH_LO()       DDRC  |= (1<<SNS)|(1<<SNSK); \
+                        PORTC &= ~((1<<SNS)|(1<<SNSK))
+#define SNS_HIZ()       DDRC  &= ~(1<<SNS)
+#define SNSK_PULSE()    PORTC |= (1<<SNSK); \
+                        DDRC  &= ~(1<<SNSK); \
+                        PORTC &= ~(1<<SNSK)
+#define SNS_LO()        DDRC  |=  (1<<PC2)
+
+#define nop( )  asm volatile ("nop\n\t" ::)
+
 // Measurement period in msec.
-#define QT_MEASUREMENT_PERIOD   25
+#define KEY_MEASUREMENT_PERIOD  25
 
 #define KEY_REPEAT_TIMEOUT      702 // Time before repeat
 #define KEY_REPEAT_DELAY        504 // Time between repeat
@@ -32,7 +53,9 @@ class Keys_t {
 private:
     Key_t Key[4];
     uint16_t Timer;
-    bool IsTouched(uint8_t ASensor) { return (qt_measure_data.qt_touch_status.sensor_states[0] & (1<<ASensor)); }
+    // Pin manipulating
+
+    //bool IsTouched(uint8_t ASensor) { return (qt_measure_data.qt_touch_status.sensor_states[0] & (1<<ASensor)); }
 public:
     void Init(void);
     void Task(void);
