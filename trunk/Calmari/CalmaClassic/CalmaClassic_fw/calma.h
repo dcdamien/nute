e7680be6c561c9e1f306974a407e97dec71e8a13
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2011-07-25 21:34:40 +0000

    CalmaClassic: I Led modified.

--- trunk/Calmari/CalmaClassic/CalmaClassic_fw/calma.h
+++ trunk/Calmari/CalmaClassic/CalmaClassic_fw/calma.h
@@ -16,30 +16,36 @@
 
 // =================================== PWM =====================================
 // Timings
-#define PWMDelayLong    108  // Low brightness
-#define PWMDelayMid     72  // Mid brightness
-#define PWMDelayFast    18  // High brightness
+#define PWMDelayLong        108  // Low brightness
+#define PWMDelayMid         72  // Mid brightness
+#define PWMDelayFast        18  // High brightness
 
 // Light constants
-#define PWM_MAX         250
-#define PWM_START_VALUE 45  // Initial LED brightness
-#define PWM_STEPOVER1   36  // Where to switch to quick PWM change mode
-#define PWM_STEPOVER2   54
+#define PWM_MAX             250
+#define PWM_START_VALUE     45  // Initial LED brightness
+#define PWM_STEPOVER1       36  // Where to switch to quick PWM change mode
+#define PWM_STEPOVER2       54
 
 #define KEY_POLL_TIMEOUT    99 // ms
 #define KEY_IS_DOWN()       bit_is_set (PINB, KEY_P)
 
 // ================================== ADC ======================================
-#define ADC_TIMEOUT     99
+#define ADC_TIMEOUT         99
 // Number of measures as power of 2: 0 means 1, 1 means 2, 3 means 8 and so on
 // Needed for averaging of values
 #define ADC_POWER_OF_MEASURES   3   // Power of 2
 #define ADC_NUMBER_OF_MEASURES  (1<<ADC_POWER_OF_MEASURES)
 
-//#define ILedNominal     400 // 20 mA @ 1.1V reference
-#define ILedNominal     504 // 25 mA @ 1.1V reference
-//#define ILedNominal     640 // 800 mA @ 2.56V reference
-//#define ILedNominal     128 // 160 mA @ 2.56V reference
+//#define ILedNominal         372 // 20 mA @ 1.1V ARef, 1R0
+//#define ILedNominal         465 // 25 mA @ 1.1V ARef, 1R0
+//#define ILedNominal         640 // 800 mA @ 2.56V ARef, 0R1
+#define ILedNominal         279 // 150 mA @ 1V1 ARef, 0R1
+
+// 1.1 V reference w/o capacitor, not left-adjusted, channel dif ADC2-ADC3 x20
+#define CALMA_ADMUX         ((0<<REFS2)|(1<<REFS1)|(0<<REFS0)|(0<<ADLAR)|(0<<MUX3)|(1<<MUX2)|(1<<MUX1)|(1<<MUX0))
+// 2.56 V reference w/o capacitor, not left-adjusted, channel dif ADC2-ADC3 x20
+//#define CALMA_ADMUX         ((1<<REFS2)|(1<<REFS1)|(0<<REFS0)|(0<<ADLAR)|(0<<MUX3)|(1<<MUX2)|(1<<MUX1)|(1<<MUX0))
+
 
 // ============================ Prototypes =====================================
 void GeneralInit(void);
