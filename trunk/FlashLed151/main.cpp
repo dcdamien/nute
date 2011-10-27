bc87232a19153a1bbf41dc3b8fd91712125a92ff
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2011-10-27 20:09:21 +0000

    FLashled

--- trunk/FlashLed151/main.cpp
+++ trunk/FlashLed151/main.cpp
@@ -14,18 +14,24 @@ int main(void) {
     // LED init
     RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
     GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_7;
+    GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_0;
     GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_OUT;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_40MHz;
     GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
     GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
     GPIO_Init(GPIOB, &GPIO_InitStructure);
 
-    GPIO_SetBits(GPIOB, GPIO_Pin_7);
+    GPIO_SetBits(GPIOB, GPIO_Pin_0);
+
+    Delay.Init();
+
+    uint32_t tmr;
 
     while (1) {
-        GPIOB->ODR ^= GPIO_Pin_7;
+        if(Delay.Elapsed(&tmr, 200)) {
+            GPIOB->ODR ^= GPIO_Pin_0;
+        }
 
-        Delay.ms(400);
+        //Delay.ms(400);
     }
 }
