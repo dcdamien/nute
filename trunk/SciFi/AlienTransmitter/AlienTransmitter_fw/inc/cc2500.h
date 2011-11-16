5af76b4136a0e73fba0142e1b164504211eb70ed
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2011-11-16 07:18:43 +0000

    ALien Transmitter board completed.

--- trunk/SciFi/AlienTransmitter/AlienTransmitter_fw/inc/cc2500.h
+++ trunk/SciFi/AlienTransmitter/AlienTransmitter_fw/inc/cc2500.h
@@ -13,33 +13,37 @@
 #include "stm32l1xx_spi.h"
 
 #include "cc2500defins.h"
+#include "arm_common.h"
+
+#define CC_MODE_RX
 
 // ============================ Types & variables ==============================
 struct CC_Packet_t {
+    uint8_t ToAddr;
     uint8_t CommandID;
     uint8_t SenderAddr;
     uint8_t SenderCycle;
     uint16_t SenderTime;
-    uint8_t ArtType;
-    uint8_t ChargeCount;
-    uint8_t RSSI;
+    int8_t RSSI;
     uint8_t LQI;
 };
-#define CC_PKT_LEN  (sizeof(CC_Packet_t)-2)
+#define CC_PKT_LEN  6//(sizeof(CC_Packet_t)-2)
 
 class CC_t {
 private:
     // Variables
     uint32_t Timer;
     uint8_t State;
-    bool GDO0_WasHi;
+    bool NewPktRcvd;
     // Methods
     uint8_t ReadWriteByte(uint8_t AByte);
     // Pins
     void CS_Hi(void) { GPIO_SetBits(GPIOA, GPIO_Pin_4); }
     void CS_Lo(void) { GPIO_ResetBits(GPIOA, GPIO_Pin_4); }
     bool GDO0_IsHi(void) { return GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_3); }
-    void BusyWait(void)  { while (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_6)); }
+    void BusyWait(void)  {} //{ while (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_6)); }
+    void IRQEnable(void);
+    void IRQDisable(void);
 
     void RfConfig(void);
     void WriteTX (uint8_t *PData, uint8_t ALength);
@@ -53,14 +57,13 @@ private:
     void Reset(void)        { WriteStrobe(CC_SRES); }
     void FlushRxFIFO(void)  { WriteStrobe(CC_SFRX); }
     void EnterTX(void)      { WriteStrobe(CC_STX);  }
-    void EnterRX(void)      { WriteStrobe(CC_SRX);  }
+    void EnterRX(void);
     void EnterIdle(void)    { WriteStrobe(CC_SIDLE);}
     void PowerDown(void)    { WriteStrobe(CC_SPWD); }
     void Calibrate(void)    { WriteStrobe(CC_SCAL); }
     void FlushTxFIFO(void)  { WriteStrobe(CC_SFTX); }
     void GetState(void)     { WriteStrobe(CC_SNOP); }
 public:
-    uint8_t Channel;
     union {
         uint8_t RX_PktArray[CC_PKT_LEN+2];
         CC_Packet_t RX_Pkt;
@@ -69,18 +72,22 @@ public:
         uint8_t TX_PktArray[CC_PKT_LEN+2];
         CC_Packet_t TX_Pkt;
     };
+    ftVoidVoid EvtNewPkt;
     // Methods
     void Init(void);
     void Task(void);
     void SetChannel(uint8_t AChannel);
+    void SetAddress(uint8_t AAddr) { WriteRegister(CC_ADDR, AAddr); }
+    // IRQ handler
+    void IRQHandler(void);
 };
 
 extern CC_t CC;
 
-
-// ================================ Prototypes =================================
-void EVENT_NewPacket(void);
-
+// Interrupt
+extern "C" {
+void EXTI3_IRQHandler(void);
+}
 
 #endif	/* _CC1101_H */
 
--- trunk/SciFi/AlienTransmitter/AlienTransmitter_fw/src/main.cpp
+++ trunk/SciFi/AlienTransmitter/AlienTransmitter_fw/src/main.cpp
@@ -8,18 +8,23 @@
 #include "stm32l1xx.h"
 #include "stm32l1xx_rcc.h"
 #include "stm32l1xx_gpio.h"
-
+#include "stm32l1xx_exti.h"
+#include "misc.h"
 #include "delay_util.h"
 #include "cc2500.h"
 #include "uart.h"
 
+void EVENT_NewPacket(void);
 void GeneralInit(void);
 
 int main(void) {
     GeneralInit();
 
+    uint32_t tmr;
     while (1) {
-
+        //CC.Task();
+        if(Delay.Elapsed(&tmr, 999))
+            klPrintf("r\r");
     }
 }
 
@@ -27,6 +32,15 @@ void GeneralInit(void) {
     Delay.Init();
     Delay.ms(63);
     UART_Init();
+    //CC.Init();
+    //CC.EvtNewPkt = EVENT_NewPacket;
+#ifdef CC_MODE_RX
+    klPrintf("\rReceiver\r");
+#else
     klPrintf("\rTransmitter\r");
+#endif
+}
 
+void EVENT_NewPacket(void) {
+    klPrintf("RSSI: %i; LQI:%u\r", (int8_t)CC.RX_Pkt.RSSI, CC.RX_Pkt.LQI);
 }
