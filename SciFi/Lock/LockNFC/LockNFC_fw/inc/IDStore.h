da01323da849c94f5ada9301bd1e86001d6ccc02
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2011-11-26 19:42:29 +0000

    Flash storage implemented

--- SciFi/Lock/LockNFC/LockNFC_fw/inc/IDStore.h
+++ SciFi/Lock/LockNFC/LockNFC_fw/inc/IDStore.h
@@ -3,6 +3,10 @@
  *
  *  Created on: 25.11.2011
  *      Author: Kreyl
+ *
+ *      Beware: number of stored IDs and Save function are made for data to fit
+ *      to (2 pages = 2048 bytes), and for page size of 1024 bytes. Change this if needed.
+ *      Payload data size is (252 * 8 bytes = 2016 bytes) + (4 bytes of count) + (4 bytes of EraseCounter) = 2024 bytes
  */
 
 #ifndef IDSTORE_H_
@@ -10,13 +14,20 @@
 
 #include <inttypes.h>
 
-#define ID_COUNT    100
+#define ID_MAX_COUNT    252 // Fit in 2*1024 bytes (2 pages)
+
+typedef struct {
+    uint32_t Count, FlashEraseCounter;
+    uint64_t ID[ID_MAX_COUNT];
+} ID_Array_t;
 
 class IDStore_t {
 private:
-    uint64_t ID[ID_COUNT];
+    ID_Array_t IDArr;
     bool IsPresentIndx(uint64_t AID, uint32_t *AIndx);
+    void dbgPrintIDs(void);
 public:
+    bool IsChanged;
     // ID operations
     bool IsPresent(uint64_t AID) { return IsPresentIndx(AID, 0); }
     void Add(uint64_t AID);
--- SciFi/Lock/LockNFC/LockNFC_fw/src/IDStore.cpp
+++ SciFi/Lock/LockNFC/LockNFC_fw/src/IDStore.cpp
@@ -6,23 +6,133 @@
  */
 
 #include "IDStore.h"
+#include "led.h"
+#include "kl_util.h"
+#include "stm32f10x.h"
+#include "stm32f10x_flash.h"
+
+#define FLASH_PAGE_SIZE     1024
+#define SAVED_DATA_SIZE     2048    // must be multiple of FlashPageSize
+#define PAGE_COUNT          (SAVED_DATA_SIZE / FLASH_PAGE_SIZE)
 
 IDStore_t IDStore;
 
+// Address of saved data
+const uint32_t SaveAddr __attribute__ ((aligned(SAVED_DATA_SIZE))) = 0;
+ID_Array_t *SavedIDs = (ID_Array_t*)&SaveAddr;
+
+void IDStore_t::dbgPrintIDs(void) {
+    klPrintf(" Count = %u\r", IDArr.Count);
+    for (uint32_t i=0; i<IDArr.Count; i++)
+        klPrintf(" %u %X%X\r", i, (uint32_t)((IDArr.ID[i] >> 32)& 0xFFFFFFFF), (uint32_t)(IDArr.ID[i] & 0xFFFFFFFF));
+}
+
 // ========================== ID operations ===================================
 bool IDStore_t::IsPresentIndx(uint64_t AID, uint32_t *AIndx) {
-    return true;
+    for (uint32_t i=0; i<IDArr.Count; i++) {
+        if (IDArr.ID[i] == AID) {
+            if (AIndx != 0) *AIndx = i;
+            klPrintf("Key indx: %u\r", i);
+            return true;
+        }
+    } // for
+    klPrintf("No such key\r");
+    return false;
 }
 void IDStore_t::Add(uint64_t AID) {
-
+    if (IsPresent(AID)) {
+        klPrintf("Key is already in base\r");
+        return;         // Already in base
+    }
+    if (IDArr.Count == ID_MAX_COUNT) {  // Base overflow, display error
+        LedRed.On();
+        Delay.ms(4005);
+        LedRed.Off();
+        klPrintf("Base overflow\r");
+        return;
+    }
+    IDArr.ID[IDArr.Count] = AID;
+    IDArr.Count++;
+    IsChanged = true;
+    klPrintf("Key added, count = %u\r", IDArr.Count);
+    //dbgPrintIDs();
 }
 void IDStore_t::Remove(uint64_t AID) {
-
+    uint32_t indx=0;
+    if (IsPresentIndx(AID, &indx)) {
+        if (indx != (IDArr.Count-1)) {  // Check if not last
+            for (uint32_t i=indx; i<IDArr.Count-1; i++) IDArr.ID[i] = IDArr.ID[i+1];
+        }
+        IDArr.Count--;
+        IsChanged = true;
+        klPrintf("Key removed, count = %u\r", IDArr.Count);
+        //dbgPrintIDs();
+    }
+    else klPrintf("No such key in base\r");
 }
+
 // Load/save
 void IDStore_t::Load(void) {
-
+    IsChanged = false;
+    IDArr.FlashEraseCounter = SavedIDs->FlashEraseCounter;
+    IDArr.Count = SavedIDs->Count;
+    klPrintf("Loading IDs...\r");
+    klPrintf("Flash Erase counter = %u\r", SavedIDs->FlashEraseCounter);
+    if (IDArr.Count == 0) {   // nothing interesting there
+        klPrintf("Flash is empty\r");
+    }
+    else {
+        for(uint32_t i=0; i<IDArr.Count; i++) IDArr.ID[i] = SavedIDs->ID[i];
+        dbgPrintIDs();
+        klPrintf("Loading completed\r\r");
+    }
 }
 void IDStore_t::Save(void) {
+    FLASH_Status FLASHStatus;
+    uint32_t FAddr = (uint32_t)&SaveAddr;
+    klPrintf("Saving IDs...\r");
+    FLASH_Unlock();
 
+    // Erase flash
+    FLASH_ClearFlag(FLASH_FLAG_EOP | FLASH_FLAG_PGERR | FLASH_FLAG_WRPRTERR);   // Clear All pending flags
+    for (uint32_t i=0; i<PAGE_COUNT; i++) {
+        FLASHStatus = FLASH_ErasePage(FAddr + (FLASH_PAGE_SIZE * i));
+        klPrintf("  Flash erase %u: %u\r", i, FLASHStatus);
+        if (FLASHStatus != FLASH_COMPLETE) return;
+    }
+
+    IDArr.FlashEraseCounter++;  // increase flash erase counter
+    // Write flash
+    uint32_t *PRAM = (uint32_t*)&IDArr;    // What to write
+    uint32_t DataWordCount = (IDArr.Count * 2) + 2; // 64-bit ID fills two 32-bit words; and two 32-bit values are added for Count and EraseCounter
+    klPrintf("  Flash write %u words: ", DataWordCount);
+    for(uint32_t i=0; i<DataWordCount; i++) {
+        FLASHStatus = FLASH_ProgramWord(FAddr, *PRAM);
+        //klPrintf("%X", *PRAM);
+        if (FLASHStatus != FLASH_COMPLETE) {
+            klPrintf("  Flash write error %u: %u\r", i, FLASHStatus);
+            return;
+        }
+        else klPrintf("#");
+        FAddr += 4;
+        PRAM++;
+    }
+    FLASH_Lock();
+
+    // Check the correctness of written data
+    uint32_t *PFLASH = (uint32_t*)&SaveAddr;
+    PRAM = (uint32_t*)&IDArr;
+    //klPrintf("\r PFLASH=%X\r", (uint32_t)PFLASH);
+    for(uint32_t i=0; i<DataWordCount; i++) {
+        //klPrintf("PRAM: %X; PFLASH: %X\r", *PRAM, *PFLASH);
+        if ((*PFLASH) != (*PRAM)) {
+            klPrintf("Data check failed at %u\r", i);
+            //return;
+        }
+        PFLASH++;
+        PRAM++;
+    } // for
+
+    klPrintf("\r  Flash write completed\r\r");
+    IsChanged = false;
 }
