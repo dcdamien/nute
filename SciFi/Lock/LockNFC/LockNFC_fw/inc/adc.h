a5a823c7c677326b210b74384920cdbdbb0c5653
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2011-11-28 08:26:46 +0000

    ADC added

---
+++ SciFi/Lock/LockNFC/LockNFC_fw/inc/adc.h
@@ -0,0 +1,35 @@
+/*
+ * adc.h
+ *
+ *  Created on: 27.11.2011
+ *      Author: Kreyl
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+#define BATTERY_DISCHARGED_U    100
+
+#define ADC_AVERAGE_COUNT       16  // Number of times to measure
+
+class Adc_t {
+private:
+    uint32_t Timer;
+    uint16_t ADCValues[ADC_AVERAGE_COUNT];
+    void Measure(void);
+protected:
+    uint16_t IValue;
+public:
+    void Init(void);
+    void Task(void);
+};
+
+class Battery_t : public Adc_t {
+public:
+    bool IsDischarged(void) { return (IValue < BATTERY_DISCHARGED_U); }
+};
+
+
+extern Battery_t Battery;
+
+#endif /* ADC_H_ */
