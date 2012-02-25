c262e3335cda72b60bdfcd632ab98f32c72a0968
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2012-02-25 15:05:54 +0000

    Cmd unit receives single letters

--- trunk/Lock/AlienLock/AlLock_fw/inc/uart_cmd.h
+++ trunk/Lock/AlienLock/AlLock_fw/inc/uart_cmd.h
@@ -11,16 +11,49 @@
 #include "stm32f10x_usart.h"
 #include "kl_lib.h"
 
+
+#define UART_BUF_SIZE    144
+
+#define FIELD_GENERATOR_TIMEOUT 18000
+
 class CmdUnit_t {
 private:
     uint32_t ITimer;
+    uint8_t TXBuf[UART_BUF_SIZE], WCounter, RCounter;
     klPwmChannel_t CoilA, CoilB;
     void WriteByte(uint8_t AByte) { USART2->DR = AByte; }
     bool ReadyToWrite(void) { return (USART2->SR & USART_FLAG_TXE); }
+    // Buffer
+    void BufWrite(uint8_t AByte) {
+        TXBuf[WCounter++] = AByte;
+        if (WCounter == UART_BUF_SIZE) WCounter = 0;
+    }
+    uint8_t BufRead(void) {
+        uint8_t IByte = TXBuf[RCounter++];
+        if (RCounter == UART_BUF_SIZE) RCounter = 0;
+        return IByte;
+    }
+    bool BufIsEmpty(void) { return (RCounter == WCounter); }
+    // Commands
+    char ICmd;
+    bool NewCmd;
+    // Printf
+    void PrintUint(uint32_t ANumber);
+    void PrintString (const char *S) {
+        while (*S != '\0') BufWrite (*S++);
+    }
+    void Print2Buf(const char *S, ...);
 public:
     void Init(void);
     void Task(void);
+    // IRQ
+    void IRQHandler(void);
 };
+
+// IRQ
+extern "C" {
+void USART2_IRQHandler(void);
+}
 
 extern CmdUnit_t CmdUnit;
 
