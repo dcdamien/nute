f9c944f2ff21f447ee6644e8300dc9d909e7f5e3
tokifuko@gmail.com <tokifuko@gmail.com@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2012-11-25 11:36:47 +0000

    Добавил tiny_sprintf и kl_assert

---
+++ trunk/HandyDevices/VaultKeeper/VaultKeeper1_fw/kl_lib/kl_assert.cpp
@@ -0,0 +1,28 @@
+#include "kl_assert.h"
+
+#include "kl_lib.h"
+//#include "lcd1200.h"
+//#include "vibro.h"
+
+
+void assertion_error(const char *filename, int line, const char *cond_string) {
+
+	// only report first violation to simplify diagnostics
+	// and avoid recursion
+	if (assertion_raised)
+		return;
+
+	assertion_raised = true;
+	/*
+	Uart.Printf("assert(%s) violated in file '%s', line %d\r",
+				cond_string, filename, line);
+	Lcd.Cls();
+	Lcd.SetDrawMode(OVERWRITE_INVERTED);
+	Lcd.Printf(0, 0, "assert(%s) violated in file '%s', line %d",
+			   cond_string, filename, line);
+	Lcd.SetDrawMode(INVERT);
+	Vibro.Flinch(4);
+	*/
+}
+
+bool assertion_raised = false;
