4b8f132a4418d0219c087c41bfa4c35b3539333d
tokifuko@gmail.com <tokifuko@gmail.com@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2012-11-28 12:21:04 +0000

    ох, забыл выжные файлы в комит добавить.

---
+++ HandyDevices/VaultKeeper/VaultKeeper1_fw/inc/GPIO_config.h
@@ -0,0 +1,31 @@
+/*
+ * GPIO_config.h
+ *
+ *  Created on: 26.11.2012
+ *      Author: Stefan
+ */
+
+#ifndef GPIO_CONFIG_H_
+#define GPIO_CONFIG_H_
+#include "stm32f10x.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_rcc.h"
+
+/* Управление драйвером 485 */
+#define DRIVER_485_EN_PORT          GPIOA
+#define DRIVER_485_EN_CLK           RCC_APB2Periph_GPIOA
+#define DRIVER_485_EN_PIN           GPIO_Pin_8
+void Driver485TxEnable(void)  ;
+void Driver485TxDisable(void)  ;
+
+/*  светодиод */
+#define LED_PORT          GPIOD
+#define LED_CLK           RCC_APB2Periph_GPIOD
+#define LED_PIN           GPIO_Pin_2
+void LedOn	(void);
+void LedOff	(void);
+
+void SimpleGPIO_Init (void);
+
+
+#endif /* GPIO_CONFIG_H_ */
