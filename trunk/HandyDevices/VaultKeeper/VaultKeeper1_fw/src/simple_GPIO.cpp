cced233228e52ede56f2638addfbeea1d0670d04
tokifuko@gmail.com <tokifuko@gmail.com@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2012-11-28 12:21:04 +0000

    ох, забыл выжные файлы в комит добавить.

---
+++ trunk/HandyDevices/VaultKeeper/VaultKeeper1_fw/src/simple_GPIO.cpp
@@ -0,0 +1,34 @@
+/*
+ * simple_GPIO.cpp
+ *
+ *  Created on: 26.11.2012
+ *      Author: Stefan
+ */
+#include "GPIO_config.h"
+
+
+void Driver485TxEnable(void)   		{GPIO_SetBits(DRIVER_485_EN_PORT,DRIVER_485_EN_PIN);}
+void Driver485TxDisable(void)   		{GPIO_ResetBits(DRIVER_485_EN_PORT,DRIVER_485_EN_PIN);}
+void LedOn	(void)					{GPIO_SetBits(LED_PORT,LED_PIN);}
+void LedOff (void)					{GPIO_ResetBits(LED_PORT,LED_PIN);}
+
+void SimpleGPIO_Init(void)
+{
+    RCC_APB2PeriphClockCmd(
+    		DRIVER_485_EN_CLK |
+    		LED_CLK |
+        RCC_APB2Periph_AFIO,
+        ENABLE );
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+    /* Configure DRIVER_485_EN as Output */
+    GPIO_InitStructure.GPIO_Pin =  DRIVER_485_EN_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_Init( DRIVER_485_EN_PORT, &GPIO_InitStructure );
+    /* Configure LED as Output */
+    GPIO_InitStructure.GPIO_Pin =  LED_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_Init( LED_PORT, &GPIO_InitStructure );
+}
