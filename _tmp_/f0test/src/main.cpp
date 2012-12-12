b643720445113611eea8cf91f125953953904f19
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2012-12-12 09:52:06 +0000

    f0test added

---
+++ _tmp_/f0test/src/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * File:   main.cpp
+ * Author: Kreyl
+ *
+ * Created on May 27, 2011, 6:37 PM
+ */
+
+#include "kl_lib_f0.h"
+#include "stm32f0xx_usart.h"
+
+void Init();
+
+int main(void) {
+    Init();
+//    Uart.Printf("klNFC\r");
+    PinSetupOut(GPIOC, 8, poPushPull, pudNone);
+    //PinSetupOut(GPIOC, 9, poPushPull, pudNone);
+    PinSet(GPIOC, 8);
+
+    uint32_t Tmr;
+    while(1) {
+        //Uart.Task();
+        if(Delay.Elapsed(&Tmr, 198)) {
+            PinToggle(GPIOC, 8);
+            //Uart.Printf("aga\r");
+            USART_SendData(USART1, 'd');
+        }
+        //Delay.ms(999);
+        //PinToggle(GPIOC, 8);
+    }
+}
+
+void Init() {
+    Delay.Init();
+    Uart.Init(115200);
+}
