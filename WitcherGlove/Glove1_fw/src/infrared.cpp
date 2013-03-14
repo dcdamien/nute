6fe9779353fece683234b2d75768e43caf75450c
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-03-14 14:45:12 +0000

    IR first step - timers synced

--- WitcherGlove/Glove1_fw/src/infrared.cpp
+++ WitcherGlove/Glove1_fw/src/infrared.cpp
@@ -12,6 +12,7 @@ Infrared_t IR;
 // Wrapper for IRQ
 extern "C" {
 void IrTxcIrq(void *p, uint32_t flags) {
+    //Uart.Printf("Irq\r");
 //    TIM5->CR1 = 0;              // Stop master timer
 //    dmaStreamDisable(IR_TX_DMA_STR);
 //    IR.IsBusy = false;
@@ -22,28 +23,34 @@ void Infrared_t::Init() {
     // ==== Carrier timer ====
     uint16_t tmp = (uint16_t)(Clk.AHBFreqHz / IR_CARRIER_HZ);   // Top timer value
     Carrier.Init(GPIOA, 8, 1, 1, tmp);
-    // Slave using TIM5 as input (ITR0), gated mode
-    //TIM1->SMCR = (0b000 << 4) | 0b101;
+    //TIM1->SMCR = (0b000 << 4) | 0b101;  // Slave using TIM5 as input (ITR0), gated mode
+    TIM1->SMCR = (0b000 << 4) | 0b100;  // Slave using TIM5 as input (ITR0), Reset mode
+    TIM1->CR2 = 0;                      // Reset is trigger output
+    TIM1->DIER = TIM_DIER_TDE;          // Request DMA on trigger
     MaxPower = tmp / 2;
-    //Carrier.On(MaxPower);   // Debug
+    Carrier.On(MaxPower);   // Debug
 
     // ==== Modulation timer ====
     rccEnableTIM5(FALSE);
     TIM5->ARR = IR_TICK_US-1;   // Delay
     //TIM5->CR2 = (0b100<<4);     // Master mode: Compare1 is used as TRGO
+    TIM5->CR2 = (0b010<<4);     // Master mode: Update is used as TRGO
     TIM5->PSC = (Clk.APB1FreqHz / 1000000) - 1; // Input Freq: 1 MHz => one tick = 1 uS
     //TIM5->DIER = TIM_DIER_UDE;  // Enable DMA request on update
-    TIM5->DIER = TIM_DIER_CC1DE;  // Enable DMA request on CH1
+    //TIM5->DIER = TIM_DIER_CC1DE;  // Enable DMA request on CH1
+    //TIM5->DIER = TIM_DIER_TDE;
 
     // Debug
     PinSetupAlterFunc(GPIOA, 0, omPushPull, pudNone, AF2);  // Debug pin
     TIM5->CCMR1 = (0b110<<4);   // PWM mode1
     TIM5->CCR1 = IR_TICK_US/2;
     TIM5->CCER = TIM_CCER_CC1E; // Enable output1
+    TIM5->CR1 = TIM_CR1_CEN;    // Enable timer, set clk division to 0, AutoReload not buffered
 
     // ==== DMA ==== Here only the unchanged parameters of the DMA are configured
     dmaStreamAllocate     (IR_TX_DMA_STR, 1, IrTxcIrq, NULL);
-    dmaStreamSetPeripheral(IR_TX_DMA_STR, Carrier.PCCR);
+    //dmaStreamSetPeripheral(IR_TX_DMA_STR, Carrier.PCCR);
+    dmaStreamSetPeripheral(IR_TX_DMA_STR, &TIM1->CCR1);
     dmaStreamSetMode      (IR_TX_DMA_STR,
             STM32_DMA_CR_CHSEL(IR_TX_DMA_CHNL)
             | DMA_PRIORITY_MEDIUM
@@ -52,14 +59,26 @@ void Infrared_t::Init() {
             | STM32_DMA_CR_MINC         // Memory pointer increase
             | STM32_DMA_CR_DIR_M2P      // Direction is memory to peripheral
             | STM32_DMA_CR_CIRC         // Enable circular buffer
-            | STM32_DMA_CR_TCIE         // Enable Transmission Complete IRQ
+           // | STM32_DMA_CR_TCIE         // Enable Transmission Complete IRQ
             );
+
+    Buf[0] = MaxPower;
+    Buf[1] = MaxPower;
+    Buf[2] = MaxPower;
+    Buf[3] = MaxPower;
+    Buf[4] = 0;
+    Buf[5] = 0;
+
+    dmaStreamSetMemory0(IR_TX_DMA_STR, Buf);
+    dmaStreamSetTransactionSize(IR_TX_DMA_STR, 6);
+    dmaStreamEnable(IR_TX_DMA_STR);
 
     // ==== Variables ====
     IsBusy = false;
 }
 
 void Infrared_t::TransmitWord(uint16_t wData, uint8_t PwrPercent) {
+  //  Uart.Printf("T\r");
 //    if(IsBusy) return;
 //    IsBusy = true;
     // Calculate power
@@ -76,18 +95,19 @@ void Infrared_t::TransmitWord(uint16_t wData, uint8_t PwrPercent) {
 //    }
 //    *p = 0;                             // Off - finishing delay
 
-    for(uint8_t i=0; i<18; i++) Buf[0] = MaxPower;
+//    for(uint8_t i=0; i<18; i++) Buf[i] = MaxPower;
+//    Uart.Printf("%A\r", Buf, 18);
+//    // Enable DMA
+//    //uint8_t CountToTx = p - Buf;
+//    dmaStreamSetMemory0(IR_TX_DMA_STR, Buf);
+//    dmaStreamSetTransactionSize(IR_TX_DMA_STR, 18);
+//    dmaStreamEnable(IR_TX_DMA_STR);
 
-    // Enable DMA
-    //uint8_t CountToTx = p - Buf;
-    dmaStreamSetMemory0   (IR_TX_DMA_STR, Buf);
-    dmaStreamSetTransactionSize(IR_TX_DMA_STR, 18);
-    dmaStreamEnable(IR_TX_DMA_STR);
 
     // Start master timer
-    TIM5->CR1 = TIM_CR1_CEN;    // Enable timer, set clk division to 0, AutoReload not buffered
-    TIM5->EGR = TIM_EGR_UG;     // Generate update: clear counter and request DMA
-    TIM5->DIER = TIM_DIER_CC1DE;  // Enable DMA request on CH1
+  //  TIM5->CR1 = TIM_CR1_CEN;    // Enable timer, set clk division to 0, AutoReload not buffered
+//    TIM5->EGR = TIM_EGR_UG;     // Generate update: clear counter and request DMA
+    //TIM5->DIER = TIM_DIER_CC1DE;  // Enable DMA request on CH1
 }
 
 
