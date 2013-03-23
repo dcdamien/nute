21b5263defeeb48ec96febbe67e815f080cdec45
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-03-23 16:51:29 +0000

    Lcd: GetBitmap & PutBitmap

--- Armlet/Armlet3/Armlet3_fw/inc/lcd2630.h
+++ Armlet/Armlet3/Armlet3_fw/inc/lcd2630.h
@@ -20,45 +20,30 @@
 #define LCD_XCS         11
 #define LCD_XRES        12
 #define LCD_MASK_WR     (0x00FF | (1<<LCD_WR))  // clear bus and set WR low
-
+#define LCD_MODE_READ   0xFFFF0000
+#define LCD_MODE_WRITE  0x00005555
 
 #define LCD_BCKLT_GPIO  GPIOB
 #define LCD_BCKLT_PIN   9
 #define LCD_BCKLT_TMR   11
 #define LCD_BCKLT_CHNL  1
 
-// Color palette: 5R-6G-5B
+// Color palette: 4R-4G-4B, 3 bytes per two pixels
 enum Color_t {
-    clBlack     = 0x0000,
-    clRed       = 0xF800,
-    clGreen     = 0x07E0,
-    clBlue      = 0x001F,
-    clYellow    = 0xFFE0,
-    clMagenta   = 0xF81F,
-    clCyan      = 0x07FF,
-    clWhite     = 0xFFFF,
-};
-
-enum PseudoGraph_t {
-    CornerTopLeftDouble = 0x99,
-    CornerTopRightDouble = 0x8B,
-    CornerBottomLeftDouble = 0x98,
-    CornerBottomRightDouble = 0x8C,
-    LineHorizDouble = 0x9D,
-    LineHorizDoubleUp = 0x9A,
-    LineHorizDoubleDown = 0x9B,
-    LineVertDouble = 0x8A,
-    LineVertDoubleLeft = 0x89,
-    LineVertDoubleRight = 0x9C,
-    LineCrossDouble = 0x9E,
+    clBlack     = 0x000,
+    clRed       = 0xF00,
+    clGreen     = 0x0F0,
+    clBlue      = 0x00F,
+    clYellow    = 0xFF0,
+    clMagenta   = 0xF0F,
+    clCyan      = 0x0FF,
+    clWhite     = 0xFFF,
 };
 
 #define LCD_X_0             1   // }
 #define LCD_Y_0             2   // } Zero pixels are shifted
-
 #define LCD_H               128 // }
 #define LCD_W               160 // } Pixels count
-
 #define LCD_TOP_BRIGHTNESS  100 // i.e. 100%
 
 class Lcd_t {
@@ -66,21 +51,18 @@ private:
     PwmPin_t BckLt;
     void WriteCmd(uint8_t ACmd);
     void WriteCmd(uint8_t ACmd, uint8_t AData);
-    void GotoXY(uint8_t x, uint8_t y);
-    uint16_t PutChar(uint8_t x, uint8_t y, char c, Color_t ForeClr, Color_t BckClr);
 public:
     // General use
     void Init();
     void Shutdown();
-    void BacklightOn(uint16_t Brightness)  { BckLt.On(Brightness); }
-    void BacklightOff() { BckLt.Off(); }
+    void Brightness(uint16_t Brightness)  { BckLt.On(Brightness); }
     // High-level
-    void Printf(uint8_t x, uint8_t y, Color_t ForeClr, Color_t BckClr, const char *S, ...);
+//    void Printf(uint8_t x, uint8_t y, Color_t ForeClr, Color_t BckClr, const char *S, ...);
     void Cls(Color_t Color);
-    void DrawImage(const uint8_t x, const uint8_t y, const uint8_t *Img);
-    void DrawSymbol(const uint8_t x, const uint8_t y, const uint8_t ACode);
-    // Symbols printing
-    void Symbols(const uint8_t x, const uint8_t y, ...);
+    void GetBitmap(uint8_t x0, uint8_t y0, uint8_t Width, uint8_t Height, uint16_t *PBuf);
+    void PutBitmap(uint8_t x0, uint8_t y0, uint8_t Width, uint8_t Height, uint16_t *PBuf);
+//    void DrawImage(const uint8_t x, const uint8_t y, const uint8_t *Img);
+//    void DrawSymbol(const uint8_t x, const uint8_t y, const uint8_t ACode);
 };
 
 extern Lcd_t Lcd;
--- Armlet/Armlet3/Armlet3_fw/src/lcd2630.cpp
+++ Armlet/Armlet3/Armlet3_fw/src/lcd2630.cpp
@@ -9,7 +9,7 @@
 
 // Variables
 Lcd_t Lcd;
-static char CharBuf[198];
+//static char CharBuf[198];
 
 // Pin driving functions
 #define LCD_DELAY   Delay_ms(1);
@@ -19,8 +19,8 @@ static inline void XCS_Hi () { PinSet  (LCD_GPIO, LCD_XCS);  LCD_DELAY}
 static inline void XCS_Lo () { PinClear(LCD_GPIO, LCD_XCS);  LCD_DELAY}
 __attribute__ ((always_inline)) static inline void DC_Hi()  { PinSet  (LCD_GPIO, LCD_DC);   LCD_DELAY}
 __attribute__ ((always_inline)) static inline void DC_Lo()  { PinClear(LCD_GPIO, LCD_DC);   LCD_DELAY}
-__attribute__ ((always_inline)) static inline void WR_Hi()  { PinSet  (LCD_GPIO, LCD_WR);   LCD_DELAY}
-__attribute__ ((always_inline)) static inline void RD_Hi()  { PinSet  (LCD_GPIO, LCD_RD);   LCD_DELAY}
+static inline void WR_Hi()   { PinSet  (LCD_GPIO, LCD_WR);   LCD_DELAY}
+static inline void RD_Hi()   { PinSet  (LCD_GPIO, LCD_RD);   LCD_DELAY}
 //__attribute__ ((always_inline)) static inline void RD_Lo()  { PinClear(LCD_GPIO, LCD_RD);   LCD_DELAY}
 
 // ==== Lcd Thread ====
@@ -45,16 +45,8 @@ void Lcd_t::Init() {
     // Configure data bus as outputs
     for(uint8_t i=0; i<8; i++) PinSetupOut(LCD_GPIO, i, omPushPull, pudNone, ps100MHz);
 
-    BacklightOn(100);
-    // Init buffer to write: add bound commands
-//    BufToWrite[0]  = ReverseAndShift(0x02A);    // Set column bounds
-//    BufToWrite[1]  = ReverseAndShift(0x100);    // 0
-//    BufToWrite[3]  = ReverseAndShift(0x100);    // 0
-//    BufToWrite[5]  = ReverseAndShift(0x02B);    // Set Row bounds
-//    BufToWrite[6]  = ReverseAndShift(0x100);    // 0
-//    BufToWrite[8]  = ReverseAndShift(0x100);    // 0
-//    BufToWrite[10] = ReverseAndShift(0x02C);    // RAM Write
     // ======= Init LCD =======
+    Brightness(LCD_TOP_BRIGHTNESS);
     XCS_Hi();
     XRES_Lo();  // }
     XRES_Hi();  // } Reset display
@@ -68,25 +60,22 @@ void Lcd_t::Init() {
     WriteCmd(0x13);         // Normal Display Mode ON
 
     //WriteCmd(0x3A, 0x06);    // Pixel format: VIPF=0(undef), IFPF=18 bit per pixel
-    WriteCmd(0x3A, 0x05);    // Pixel format: VIPF=0(undef), IFPF=16 bit per pixel
-    //WriteCmd(0x3A, 0x03);   // Pixel format: VIPF=0(undef), IFPF=12 bit per pixel
+    //WriteCmd(0x3A, 0x05);    // Pixel format: VIPF=0(undef), IFPF=16 bit per pixel
+    WriteCmd(0x3A, 0x03);   // Pixel format: VIPF=0(undef), IFPF=12 bit per pixel
 
     WriteCmd(0x29);         // Display on
     WriteCmd(0x20);         // Inv off
     WriteCmd(0x13);         // Normal Display Mode ON
     WriteCmd(0x36, 0xA0);   // Display mode: Y inv, X none-inv, Row/Col exchanged
-    //Cls(clBlue);
 
-    PinSetupOut(GPIOD, 6, omPushPull);
-    while(1) {
-        PinSet(GPIOD, 6);
-        Cls(clGreen);
-        PinClear(GPIOD, 6);
-        Cls(clRed);
-    }
-    //Cls(clWhite);
+    Cls(clBlack);
 
+    PutBitmap(45, 45, 10, 10, (uint16_t*)0x08000000);
 
+    uint16_t Buf[120];
+    GetBitmap(45, 45, 10, 10, Buf);
+    for(uint8_t i=0; i<100; i++) Uart.Printf("%04X ", Buf[i]);
+    Uart.Printf("\r");
     // ======= Create and start thread =======
     //chThdCreateStatic(waLcdThread, sizeof(waLcdThread), NORMALPRIO, LcdThread, NULL);
 }
@@ -94,14 +83,28 @@ void Lcd_t::Init() {
 void Lcd_t::Shutdown(void) {
     XRES_Lo();
     XCS_Lo();
-    BacklightOff();
+    Brightness(0);
 }
 
 // =============================== Local use ===================================
+__attribute__ ((always_inline)) static inline void ModeWrite() {
+    LCD_GPIO->MODER |= LCD_MODE_WRITE;
+}
+__attribute__ ((always_inline)) static inline void ModeRead() {
+    LCD_GPIO->MODER &= LCD_MODE_READ;
+}
+
 __attribute__ ((always_inline)) static inline void WriteByte(uint8_t Byte) {
     LCD_GPIO->BSRRH = LCD_MASK_WR;  // Clear bus and set WR Low
     LCD_GPIO->BSRRL = Byte;         // Place data on bus
     LCD_GPIO->BSRRL = (1<<LCD_WR);  // WR high
+}
+__attribute__ ((always_inline)) static inline uint8_t ReadByte() {
+    uint16_t w;
+    LCD_GPIO->BSRRH = (1<<LCD_RD);  // RD Low
+    LCD_GPIO->BSRRL = (1<<LCD_RD);  // RD high
+    w = LCD_GPIO->IDR;              // Read data from bus
+    return (uint8_t)w;
 }
 
 // ==== WriteCmd ====
@@ -109,7 +112,6 @@ void Lcd_t::WriteCmd(uint8_t ACmd) {
     // DC is lo by default => Cmd by default
     WriteByte(ACmd);    // Send Cmd byte
 }
-
 void Lcd_t::WriteCmd(uint8_t ACmd, uint8_t AData) {
     // DC is lo by default => Cmd by default
     WriteByte(ACmd);    // Send Cmd byte
@@ -119,32 +121,11 @@ void Lcd_t::WriteCmd(uint8_t ACmd, uint8_t AData) {
     DC_Lo();
 }
 
-/* ==== Pseudographics ====
- *  Every command consists of PseudoGraph_t AChar, uint8_t RepeatCount.
- *  Example: LineHorizDouble, 11 => print LineHorizDouble 11 times.
- *  Cmds end up with 0.
- */
-/*
-void Lcd_t::Symbols(const uint8_t x, const uint8_t y, ...) {
-    GotoCharXY(x, y);
-    uint8_t FCharCode=1, RepeatCount;
-    va_list Arg;
-    va_start(Arg, y);    // Set pointer to last argument
-    while(1) {
-        FCharCode = (uint8_t)va_arg(Arg, int32_t);
-        if(FCharCode == 0) break;
-        RepeatCount = (uint8_t)va_arg(Arg, int32_t);
-        for(uint8_t j=0; j<RepeatCount; j++) DrawChar(FCharCode, NotInverted);
-    }
-    va_end(Arg);
-}
-
-*/
 // ================================= Printf ====================================
-uint16_t Lcd_t::PutChar(uint8_t x, uint8_t y, char c, Color_t ForeClr, Color_t BckClr) {
-    char *PFont = (char*)Font8x8;  // Font to use
-    // Read font params
-    uint8_t nCols = PFont[0];
+//uint16_t Lcd_t::PutChar(uint8_t x, uint8_t y, char c, Color_t ForeClr, Color_t BckClr) {
+//    char *PFont = (char*)Font8x8;  // Font to use
+//    // Read font params
+//    uint8_t nCols = PFont[0];
 //    uint8_t nRows = PFont[1];
 //    uint16_t nBytes = PFont[2];
     // Get pointer to the first byte of the desired character
@@ -171,103 +152,96 @@ uint16_t Lcd_t::PutChar(uint8_t x, uint8_t y, char c, Color_t ForeClr, Color_t B
 //        for(col = xaStart; col<=xaEnd; col++)
 //            Changed[row][col] = true;
 //    // Return next pixel to right
-    return x+nCols;
-}
+//    return x+nCols;
+//}
 
-void Lcd_t::Printf(uint8_t x, uint8_t y, const Color_t ForeClr, const Color_t BckClr, const char *S, ...) {
-    // Printf to buffer
-    va_list args;
-    va_start(args, S);
-    uint32_t Cnt = tiny_vsprintf(CharBuf, S, args);
-    va_end(args);
-    // Draw what printed
-    for(uint32_t i=0; i<Cnt; i++) {
-        x = PutChar(x, y, CharBuf[i], ForeClr, BckClr);
-        if(x>160) break;
-    }
-}
+//void Lcd_t::Printf(uint8_t x, uint8_t y, const Color_t ForeClr, const Color_t BckClr, const char *S, ...) {
+//    // Printf to buffer
+//    va_list args;
+//    va_start(args, S);
+//    uint32_t Cnt = tiny_vsprintf(CharBuf, S, args);
+//    va_end(args);
+//    // Draw what printed
+//    for(uint32_t i=0; i<Cnt; i++) {
+//        x = PutChar(x, y, CharBuf[i], ForeClr, BckClr);
+//        if(x>160) break;
+//    }
+//}
 
 // ================================ Graphics ===================================
-void Lcd_t::Cls(Color_t Color) {
+__attribute__ ((always_inline)) static inline void SetBounds(uint8_t xStart, uint8_t xEnd, uint8_t yStart, uint8_t yEnd) {
     // Set column bounds
     WriteByte(0x2A);
     DC_Hi();
     WriteByte(0x00);            // }
-    WriteByte(LCD_X_0);         // } Col addr start = 0
+    WriteByte(LCD_X_0+xStart);  // } Col addr start
     WriteByte(0x00);            // }
-    WriteByte(LCD_X_0+LCD_W-1); // } Col addr end
+    WriteByte(LCD_X_0+xEnd-1);  // } Col addr end
     DC_Lo();
     // Set row bounds
     WriteByte(0x2B);
     DC_Hi();
     WriteByte(0x00);            // }
-    WriteByte(LCD_Y_0);         // } Row addr start = 0
+    WriteByte(LCD_Y_0+yStart);  // } Row addr start = 0
     WriteByte(0x00);            // }
-    WriteByte(LCD_Y_0+LCD_H-1); // } Row addr end
+    WriteByte(LCD_Y_0+yEnd-1);  // } Row addr end
     DC_Lo();
+}
+
+void Lcd_t::Cls(Color_t Color) {
+    SetBounds(0, LCD_W, 0, LCD_H);
+    // Prepare variables
+    uint16_t Clr = (uint16_t)Color;
+    uint32_t Cnt = LCD_W * LCD_H / 2;       // Two pixels at one time
+    uint8_t b1 = (uint8_t)(Clr >> 4);       // RRRR-GGGG
+    uint8_t b2 = (uint8_t)(((Clr & 0x00F) << 4) | (Clr >> 8));  // BBBB-RRRR
+    uint8_t b3 = (uint8_t)(Clr & 0x0FF);    // GGGG-BBBB
     // Write RAM
-    uint8_t MSB = (uint8_t)(((uint16_t)Color) >> 8);
-    uint8_t LSB = (uint8_t)(((uint16_t)Color) & 0x00FF);
     WriteByte(0x2C);    // Memory write
     DC_Hi();
-    for(uint8_t x=0; x<LCD_W; x++) {
-        for(uint8_t y=0; y<LCD_H; y++) {
-            WriteByte(MSB);
-            WriteByte(LSB);
-        }
+    for(uint32_t i=0; i<Cnt; i++) {
+        WriteByte(b1);
+        WriteByte(b2);
+        WriteByte(b3);
     }
     DC_Lo();
 }
-/*
-void Lcd_t::GotoXY(uint8_t x, uint8_t y) {
-    CurrentPosition =  x;               // move to x
-    CurrentPosition += (y<<6)+(y<<5);   // = (*64)+(*32) = *96; move to y
+
+void Lcd_t::GetBitmap(uint8_t x0, uint8_t y0, uint8_t Width, uint8_t Height, uint16_t *PBuf) {
+    SetBounds(x0, x0+Width-1, y0, y0+Height-1);
+    // Prepare variables
+    uint32_t Cnt = Width * Height;
+    uint16_t R, G, B;
+    // Read RAM
+    WriteByte(0x2E);    // RAMRD
+    DC_Hi();
+    ModeRead();
+    ReadByte();         // Dummy read
+    for(uint32_t i=0; i<Cnt; i++) {
+        R = ReadByte(); // }
+        G = ReadByte(); // }
+        B = ReadByte(); // } Inside LCD, data is always in 18bit format.
+        // Produce 4R-4G-4B from 6R-6G-6B
+        *PBuf++ = ((R & 0xF0) << 4) | (G & 0xF0) | ((B & 0xF0) >> 4);
+    }
+    ModeWrite();
+    DC_Lo();
 }
 
-void Lcd_t::DrawChar(uint8_t AChar, Invert_t AInvert) {
-    uint8_t b;
-    for(uint8_t i=0; i<6; i++) {
-        b = Font_6x8_Data[AChar][i];
-        if(AInvert == Inverted) b = ~b;
-        IBuf[CurrentPosition++] = b;
-        if (CurrentPosition >= LCD_VIDEOBUF_SIZE) CurrentPosition = 0;
+void Lcd_t::PutBitmap(uint8_t x0, uint8_t y0, uint8_t Width, uint8_t Height, uint16_t *PBuf) {
+    SetBounds(x0, x0+Width-1, y0, y0+Height-1);
+    // Prepare variables
+    uint16_t Clr1, Clr2;
+    uint32_t Cnt = Width * Height / 2;      // Two pixels at one time
+    // Write RAM
+    WriteByte(0x2C);    // Memory write
+    DC_Hi();
+    for(uint32_t i=0; i<Cnt; i++) {
+        Clr1 = *PBuf++;
+        Clr2 = *PBuf++;
+        WriteByte(Clr1 >> 4);    // RRRR-GGGG
+        WriteByte(((Clr1 & 0x00F) << 4) | (Clr2 >> 8));   // BBBB-RRRR
+        WriteByte(Clr2 & 0x0FF); // GGGG-BBBB
     }
+    DC_Lo();
 }
-
-void Lcd_t::DrawImage(const uint8_t x, const uint8_t y, const uint8_t* Img) {
-    uint16_t i=0;
-    uint8_t Width = Img[i++], Height = Img[i++];
-    for(uint8_t fy=y; fy<y+Height; fy++) {
-        GotoXY(x, fy);
-        for(uint8_t fx=x; fx<x+Width; fx++) {
-            IBuf[CurrentPosition++] = Img[i++];
-            if (CurrentPosition >= LCD_VIDEOBUF_SIZE) continue;
-        } // fx
-    } // fy
-}
-
-void Lcd_t::DrawSymbol(const uint8_t x, const uint8_t y, const uint8_t ACode) {
-    uint16_t Pos;
-    uint8_t b;
-    // First row
-    uint8_t Row = y/8;
-    uint8_t ry = y - Row*8;         // y within row
-    Pos = x + (Row<<6) + (Row<<5);  // Move to position
-    for (uint8_t xi=0; xi<6; xi++) {
-        b = Font_6x8_Data[ACode][xi];
-        b <<= ry;
-        IBuf[Pos++] |= b;
-        if (Pos >= LCD_VIDEOBUF_SIZE) Pos = 0;
-    }
-    // Second row
-    Row++;
-    ry = 8-ry;
-    Pos = x + (Row<<6) + (Row<<5);  // Move to position
-    for (uint8_t xi=0; xi<6; xi++) {
-        b = Font_6x8_Data[ACode][xi];
-        b >>= ry;
-        IBuf[Pos++] |= b;
-        if (Pos >= LCD_VIDEOBUF_SIZE) Pos = 0;
-    }
-}
-*/
