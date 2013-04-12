ee1d054fc810cb66d8f4d8dc99ce7e62794b2daf
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-04-12 19:57:45 +0000

    evt_mask.h added

---
+++ trunk/Armlet/Armlet3/Armlet3_fw/inc/evt_mask.h
@@ -0,0 +1,16 @@
+/*
+ * evt_mask.h
+ *
+ *  Created on: 12.04.2013
+ *      Author: kreyl
+ */
+
+#ifndef EVT_MASK_H_
+#define EVT_MASK_H_
+
+// Masks for events registering and processing
+#define EVTMASK_RADIO_RX    (1<<0)
+#define EVTMASK_RADIO_TX    (1<<1)
+#define EVTMASK_KEYS        (1<<2)
+
+#endif /* EVT_MASK_H_ */
