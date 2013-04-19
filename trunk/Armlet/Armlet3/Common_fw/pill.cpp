1b5cd233271577767ca1f7f58725d78d87d9462f
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-04-19 12:14:26 +0000

    i2c read/write completed (needed for Pill)

--- trunk/Armlet/Armlet3/Common_fw/pill.cpp
+++ trunk/Armlet/Armlet3/Common_fw/pill.cpp
@@ -15,12 +15,18 @@ static i2c_t i2c;
 void PillReset();
 
 // =============================== Thread ======================================
-static WORKING_AREA(waPillThread, 128);
+static WORKING_AREA(waPillThread, 256);
 static msg_t PillThread(void *arg) {
     (void)arg;
     chRegSetThreadName("Pill");
 
-    uint8_t Rslt;
+    //uint8_t Rslt;
+    uint8_t Buf[7];
+    Buf[0] = 9;
+    Buf[1] = 0xA5;
+    Pill[0].Write(Buf, 4);
+    Buf[0] = 0;
+    Buf[1] = 0;
 
     while(1) {
         chThdSleepMilliseconds(27);
@@ -33,8 +39,10 @@ static msg_t PillThread(void *arg) {
 //        bool OldState, HasChanged = false;
 //        for(uint8_t i=0; i<PILL_CNT; i++) {
 //            OldState = Pill[i].Connected;       // Save current state
-        Rslt = Pill[0].Read(NULL, 0);              // Try to read pill
-        if(Rslt == OK) Uart.Printf("Ok\r");
+
+        if(Pill[0].Read(Buf, 4) == OK) Uart.Printf("%A\r", Buf, 4, ' ');
+//        Rslt = Pill[0].CheckIfConnected();
+//        if(Rslt == OK) Uart.Printf("Ok\r");
 
 //            if(Pill[i].Connected != OldState) HasChanged = true;
 //        } // for
@@ -55,7 +63,7 @@ void PillInit() {
     PinSetupOut(PERIPH_PWR_GPIO, PERIPH_PWR_PIN, omPushPull);   // Power
     PinSet(PERIPH_PWR_GPIO, PERIPH_PWR_PIN);
     chThdSleepMilliseconds(1);  // Allow power to rise
-    i2c.Init(PILL_I2C, PILL_I2C_GPIO, PILL_SCL_PIN, PILL_SDA_PIN, PILL_I2C_BITRATE_HZ);
+    i2c.Init(PILL_I2C, PILL_I2C_GPIO, PILL_SCL_PIN, PILL_SDA_PIN, PILL_I2C_BITRATE_HZ, PILL_DMATX, PILL_DMARX);
     // Firmware
     chEvtInit(&IEvtPillChange);
     // Pills
@@ -80,14 +88,37 @@ void PillRegisterEvtChange(EventListener *PEvtLstnr, uint8_t EvtMask) {
 void Pill_t::Init(uint8_t IcAddr) {
     IAddr = IcAddr;
     Connected = false;
+    chBSemInit(&ISem, NOT_TAKEN);
 }
 
 uint8_t Pill_t::Read(uint8_t *Ptr, uint8_t Length) {
-    chBSemInit(&ISem, NOT_TAKEN);
     if(chBSemWaitTimeout(&ISem, TIME_INFINITE) != RDY_OK) return FAILURE;
     uint8_t WordAddress = PILL_START_ADDR;
     uint8_t Rslt = i2c.CmdWriteRead(EEADDR+IAddr, &WordAddress, 1, Ptr, Length);
+    Connected = (Rslt == OK);
     if(i2c.Error == true) PillReset();
     chBSemSignal(&ISem);
     return Rslt;
+}
+
+uint8_t Pill_t::CheckIfConnected() {
+    if(chBSemWaitTimeout(&ISem, TIME_INFINITE) != RDY_OK) return FAILURE;
+    uint8_t Rslt = i2c.CmdWriteWrite(EEADDR+IAddr, NULL, 0, NULL, 0);
+    Connected = (Rslt == OK);
+    if(i2c.Error == true) PillReset();
+    chBSemSignal(&ISem);
+    return Rslt;
+}
+
+// TODO: write by pages
+uint8_t Pill_t::Write(uint8_t *Ptr, uint8_t Length) {
+    if(chBSemWaitTimeout(&ISem, TIME_INFINITE) != RDY_OK) return FAILURE;
+    uint8_t WordAddress = PILL_START_ADDR;
+    uint8_t Rslt = i2c.CmdWriteWrite(EEADDR+IAddr, &WordAddress, 1, Ptr, Length);
+    Connected = (Rslt == OK);
+    if(Rslt == OK) chThdSleepMilliseconds(5);   // Allow memory to complete writing
+    if(i2c.Error == true) PillReset();
+    chBSemSignal(&ISem);
+    return Rslt;
+
 }
