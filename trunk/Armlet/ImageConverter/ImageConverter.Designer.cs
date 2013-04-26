98e623e74806777a88c715f850f099ca03a104f5
alexkrivda@gmail.com <alexkrivda@gmail.com@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-04-26 21:29:49 +0000

    Image Converter

---
+++ trunk/Armlet/ImageConverter/ImageConverter.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ImageConverter
+{
+    partial class ImageConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtContent
+            // 
+            this.txtContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtContent.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtContent.Location = new System.Drawing.Point(276, 0);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtContent.Size = new System.Drawing.Size(472, 355);
+            this.txtContent.TabIndex = 1;
+            this.txtContent.WordWrap = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.txtFile);
+            this.panel1.Controls.Add(this.pictureBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(276, 355);
+            this.panel1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(12, 3);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(218, 20);
+            this.txtFile.TabIndex = 5;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 32);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(250, 164);
+            this.pictureBox.TabIndex = 4;
+            this.pictureBox.TabStop = false;
+            // 
+            // ImageConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 355);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.panel1);
+            this.Name = "ImageConverter";
+            this.Text = "ImageConverter";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ trunk/Armlet/ImageConverter/ImageConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImageConverter
+{
+    public partial class ImageConverter : Form
+    {
+        public ImageConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            try
+            {
+                var bmp = new Bitmap(openFileDialog1.FileName);
+                pictureBox.Image = bmp;
+                txtContent.Text = string.Empty;
+                txtContent.Text += String.Format("const uword_t {0}Height = {1};{3}const uword_t {0}Width = {2};",
+                                                 Path.GetFileNameWithoutExtension(openFileDialog1.FileName),
+                                                 pictureBox.Image.Height, pictureBox.Image.Width, Environment.NewLine
+                    );
+
+                var sb = new StringBuilder(pictureBox.Image.Width*pictureBox.Image.Height*4*5);
+                for (int i = 0; i < pictureBox.Image.Height; i++)
+                {
+                    sb.Append(Environment.NewLine);
+                    for (int j = 0; j < pictureBox.Image.Height; j++)
+                    {
+                        sb.AppendFormat("{0}, {1}, {2}, {3},", bmp.GetPixel(i, j).A, bmp.GetPixel(i, j).R,
+                                        bmp.GetPixel(i, j).G, bmp.GetPixel(i, j).B);
+                    }
+                        
+                }
+                //last comma
+                sb.Remove(sb.Length - 2, 1);
+                txtContent.Text += String.Format("sbyte_t {0}Bitmap[]={{{1}}};",
+                                                 Path.GetFileNameWithoutExtension(openFileDialog1.FileName),
+                                                 sb);
+
+            }
+            catch (Exception ex)
+            {
+                    
+                MessageBox.Show(@"Can't load file: " + ex.Message);
+            }
+        }
+    }
+}
