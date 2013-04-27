f3433268ef2e6c9d50b5dd640dab5c1f6ef3e248
keltur77 <keltur77@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-04-27 21:17:04 +0000

    prototypes

--- Armlet/ArmletShell/LowLevelLib/UpInterface.h
+++ Armlet/ArmletShell/LowLevelLib/UpInterface.h
@@ -8,5 +8,7 @@ void Clear(short backColor);
 void DrawTextString(int x, int y, const char* string, int sz, short foreColor, short backColor);
 void RegisterButtonHandlers(BUTTON_HANDLER ClickButtonHandler,BUTTON_HANDLER HoldButtonHandler);
 void DrawRect_kel(int x, int y, int sx, int sy, short color);
+void DrawBitmap_kel(int x, int y, int sx, int sy, short* bitmap);
+void DrawBitmapRect_kel(int x, int y, int sx, int sy, short* bitmap, int dx, int dy, int sdx, int sdy);
 bool RequestTimer(int period, TIMER_PROC routine);
 void DrawPixel(int x, int y, short c);
