c02419aaa10e7388ed153d8aad825b2cff398fa0
keltur77 <keltur77@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-04-27 22:57:08 +0000

    16 ARGB, fixies

--- trunk/Armlet/ImageConverter/ImageConverter.cs
+++ trunk/Armlet/ImageConverter/ImageConverter.cs
@@ -22,28 +22,40 @@ namespace ImageConverter
                 var bmp = new Bitmap(openFileDialog1.FileName);
                 pictureBox.Image = bmp;
                 txtContent.Text = string.Empty;
-                txtContent.Text += String.Format("const uword_t {0}Height = {1};{3}const uword_t {0}Width = {2};",
-                                                 Path.GetFileNameWithoutExtension(openFileDialog1.FileName),
-                                                 pictureBox.Image.Height, pictureBox.Image.Width, Environment.NewLine
+                txtContent.Text += String.Format(
+                    "const uword_t {0}Height = {1};{3}const uword_t {0}Width = {2};{3}",
+                        Path.GetFileNameWithoutExtension(openFileDialog1.FileName),
+                        pictureBox.Image.Height, 
+                        pictureBox.Image.Width, 
+                        Environment.NewLine
                     );
 
-                var sb = new StringBuilder(pictureBox.Image.Width*pictureBox.Image.Height*4*5);
-                for (int i = 0; i < pictureBox.Image.Height; i++)
+                var sb = new StringBuilder(pictureBox.Image.Width*pictureBox.Image.Height*8);
+                for (int j = 0; j < pictureBox.Image.Height; j++)
                 {
                     sb.Append(Environment.NewLine);
-                    for (int j = 0; j < pictureBox.Image.Height; j++)
+                    for (int i = 0; i < pictureBox.Image.Width; i++)
                     {
-                        sb.AppendFormat("{0}, {1}, {2}, {3},", bmp.GetPixel(i, j).A, bmp.GetPixel(i, j).R,
-                                        bmp.GetPixel(i, j).G, bmp.GetPixel(i, j).B);
+                        int chA = (bmp.GetPixel(i, j).A >> 4) & 0x0F;
+                        int chR = (bmp.GetPixel(i, j).R >> 4) & 0x0F;
+                        int chG = (bmp.GetPixel(i, j).G >> 4) & 0x0F;
+                        int chB = (bmp.GetPixel(i, j).B >> 4) & 0x0F;
+
+                        int color = (chA << 12)|
+                                    (chR <<  8)|
+                                    (chG <<  4)|
+                                    (chB <<  0);
+
+                        sb.AppendFormat("0x{0:X4}, ",color);
                     }
                         
                 }
                 //last comma
                 sb.Remove(sb.Length - 2, 1);
-                txtContent.Text += String.Format("sbyte_t {0}Bitmap[]={{{1}}};",
-                                                 Path.GetFileNameWithoutExtension(openFileDialog1.FileName),
-                                                 sb);
-
+                txtContent.Text += String.Format("const uword_t {0}Bitmap[]={{{1}{2}}};",
+                        Path.GetFileNameWithoutExtension(openFileDialog1.FileName),
+                        sb,
+                        Environment.NewLine);
             }
             catch (Exception ex)
             {
