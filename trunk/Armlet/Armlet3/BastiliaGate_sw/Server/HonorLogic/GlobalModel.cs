97c715ebe24e7661bc909cf3a6ca223533639835
leo@bastilia.ru <leo@bastilia.ru@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-04-28 12:47:48 +0000

    Gate UI

--- trunk/Armlet/Armlet3/BastiliaGate_sw/Server/HonorLogic/GlobalModel.cs
+++ trunk/Armlet/Armlet3/BastiliaGate_sw/Server/HonorLogic/GlobalModel.cs
@@ -9,28 +9,54 @@ namespace HonorLogic
     {
         public GlobalModel(IArmletDeliveryServece service)
         {
-            service.ArmletsStatusUpdate +=service_ArmletsStatusUpdate;
+            _service = service;
+            _service.ArmletsStatusUpdate +=service_ArmletsStatusUpdate;
         }
 
         private void service_ArmletsStatusUpdate(PlayerStatusUpdate[] obj)
         {
+            var raiseListUpdated = false;
             lock (_syncRoot)
             {
                 foreach (var playerStatusUpdate in obj)
                 {
-                    
+                    byte armletId = playerStatusUpdate.armlet_id;
+                    if (!_armlets.ContainsKey(armletId))
+                    {
+                        raiseListUpdated = true;
+                        _armlets.Add(armletId, CreateArmlet(armletId));
+                    }
+                    var armlet = _armlets[armletId];
+                    armlet.BloodLevel = playerStatusUpdate.new_blood;
+                    armlet.Room = playerStatusUpdate.new_room;
                 }
+            }
+            if (raiseListUpdated)
+            {
+                OnArmletListUpdated();
             }
         }
 
+        private Armlet CreateArmlet(byte armletId)
+        {
+            return new Armlet(armletId, _service);
+        }
+
         private readonly object _syncRoot = new object();
-        private readonly List<IArmletInfo> _armlets = new List<IArmletInfo>();
+        private readonly Dictionary<int, Armlet> _armlets = new Dictionary<int, Armlet>();
+        private readonly IArmletDeliveryServece _service;
+
         public IEnumerable<IArmletInfo> GetArmlets()
         {
-            return _armlets;
+            return _armlets.Values;
         }
 
         public event Action ArmletListUpdated;
+        public event Action<byte> NewGateOnline;
+        public IGateModel GetGateModel(byte gateId)
+        {
+            throw new NotImplementedException();
+        }
 
         private void OnArmletListUpdated()
         {
---
+++ trunk/Armlet/Armlet3/BastiliaGate_sw/Server/ServerGUI/GateTabControl.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PillInterfaces;
+
+namespace ServerGUI
+{
+    /// <summary>
+    /// Interaction logic for GateTabControl.xaml
+    /// </summary>
+    public partial class GateTabControl : UserControl
+    {
+        public IGlobalModel Model { get; set; }
+
+        private readonly List<byte> _gates = new List<byte>();
+        public GateTabControl()
+        {
+            InitializeComponent();
+        }
+
+        private void GateTabControl_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Model.NewGateOnline += (obj) => Dispatcher.BeginInvoke(new Action<byte>(Model_NewGateOnline));
+        }
+
+        private readonly object _syncRoot = new object();
+
+        void Model_NewGateOnline(byte gateId)
+        {
+            lock (_syncRoot)
+            {
+                if (!_gates.Contains(gateId))
+                {
+                    _gates.Add(gateId);
+                    var gateUi = new GateUI {Model = Model.GetGateModel(gateId)};
+                    GatesTabControl.Items.Add(new TabItem {Content = gateUi});
+                }
+            }
+        }
+    }
+}
