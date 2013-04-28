45e08a321c2d9a7f2d726fbe1ba752c3315f72bb
leo@bastilia.ru <leo@bastilia.ru@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-04-28 16:10:45 +0000

    Correctly handle gate not connected

---
+++ trunk/Armlet/Armlet3/BastiliaGate_sw/Server/PillInterfaces/GateNotConnectedException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PillInterfaces
+{
+    public class GateNotConnectedException : Exception
+    {
+        public GateNotConnectedException(byte gateId)
+            : base("Gate with id " + gateId + " is not connected")
+        {
+        }
+    }
+}
