fc88582e4d3bc056b95e6536392a4106e73ade1a
keltur77@gmail.com <keltur77@gmail.com@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-04-29 18:13:39 +0000

    small changes in API
    added empty CALLBACK/CALLOUT/SYSCALL/NOCALL/FUTURE qualifiers
    
    marks api for zamoks wiht FUTURE
    added GetBatteryLevel, GetRandom, GetUpTime and sprintf to API

--- trunk/Armlet/ArmletShell/LowLevelLib/PlatformDependentSizes.h
+++ trunk/Armlet/ArmletShell/LowLevelLib/PlatformDependentSizes.h
@@ -4,10 +4,19 @@
 	#define PFD_SIZES
 	#define MUTEX_SIZE sizeof(void*)
 	#define FILE_SIZE sizeof(void*)
+	#define SYSCALL	__cdecl
+	#define CALLOUT	__cdecl
+	#define NOCALL  __cdecl
+	#define FUTURE	__cdecl
+	#define CALLBACK __cdecl
 #endif
 
 #ifndef _MBCS
-	#define __stdcall
+	#define NOCALL
+	#define CALLOUT
+	#define SYSCALL
+	#define FUTURE
+	#define CALLBACK
 #endif
 
 //TODO KREYL - add CHIBI OS sizes
