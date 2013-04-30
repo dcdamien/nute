b3e52960f940f6090cfe2b3f6c897923765e94b6
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-04-30 14:36:53 +0000

    proceeding with API implementation

--- trunk/Armlet/Armlet3/Armlet3_fw/inc/keys.h
+++ trunk/Armlet/Armlet3/Armlet3_fw/inc/keys.h
@@ -14,29 +14,16 @@
 // Keys ports and pins
 #define KEY_GPIO    GPIOD
 const uint16_t KeyPin[] = {
-        0,  // 1 = A
-        1,  // 2 = B
-        3,  // 3 = C
+        0,  // A
+        1,  // B
+        3,  // C
+        9,  // X
+        10, // Y
+        11, // Z
         4,  // 4
         7,  // 5
         8,  // 6
-        9,  // 7 = X
-        10, // 8 = Y
-        11, // 9 = Z
 };
-
-// Key numbers
-/*
-#define KEY_A   0x001
-#define KEY_B   0x002
-#define KEY_C   0x004
-#define KEY_L   0x008
-#define KEY_M   0x010
-#define KEY_R   0x020
-#define KEY_X   0x040
-#define KEY_Y   0x080
-#define KEY_Z   0x100
-*/
 
 #define KEYS_CNT    9
 #define KEYS_POLL_PERIOD_MS     72
