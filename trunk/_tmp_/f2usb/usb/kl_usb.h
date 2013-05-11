b0ea9a6f1436e16962f52b0877e7c24711611074
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-05-11 07:25:19 +0000

    Still resets after response to GetDescriptor

--- trunk/_tmp_/f2usb/usb/kl_usb.h
+++ trunk/_tmp_/f2usb/usb/kl_usb.h
@@ -11,14 +11,33 @@
 #include "kl_lib_f2xx.h"
 
 #define RX_FIFO_SZ      (STM32_USB_OTG1_RX_FIFO_SIZE / 4)
-#define EP_BUF_SZ       64
+#define EP0_SZ          64
 #define SETUP_BUF_SZ    8   // Standard value, do not change
 
-enum EpMode_t {emControl, emIn, emOut};
+//enum EpMode_t {emControl, emIn, emOut};
 
-struct Endpoint_t {
-    EpMode_t Mode;
-    uint8_t Buf[EP_BUF_SZ];
+typedef void (*EpCallback_t)(void);
+
+class Endpoint_t {
+private:
+    uint8_t *IPBuf;
+    uint32_t ILen;
+public:
+    bool IsTransmitting, IsReceiving, IsTxPending;
+    uint8_t SelfN;
+    uint8_t InMaxSz, OutMaxSz;
+    EpCallback_t cbIn, cbOut;
+    // Transfer section
+    uint8_t *PTxBuf;
+    uint32_t TxSize;
+    bool TxFifoHandler();
+    // Methods
+    void StallIn()  { OTG_FS->ie[SelfN].DIEPCTL |= DIEPCTL_STALL; }
+    void StallOut() { OTG_FS->oe[SelfN].DOEPCTL |= DOEPCTL_STALL; }
+    void PrepareTransmit(uint8_t *Ptr, uint32_t Len);
+    void PrepareReceive(uint32_t Len);
+    void Transmit(uint8_t *Ptr, uint32_t Len);
+    void Receive(uint32_t Len);
 };
 
 class UsbMemAllocator_t {
@@ -41,8 +60,10 @@ private:
     Endpoint_t Ep[USB_EP_CNT];
     uint8_t SetupBuf[SETUP_BUF_SZ];
     void Ep0SetupHandler();
-    uint8_t StdRequestHandler();
+    uint8_t StdRequestHandler(uint8_t **Ptr, uint32_t *PLen);
     // Common
+    uint16_t IStatus;
+    uint8_t Configuration;
     void RxHandler();
     void SetAddress(uint8_t Addr) { OTG_FS->DCFG = (OTG_FS->DCFG & ~DCFG_DAD_MASK) | DCFG_DAD(Addr); }
     // Buffer operations
@@ -53,6 +74,7 @@ private:
     // irq methods
     void IReset();
     void IEpOutHandler(uint8_t EpN);
+    void IEpInHandler(uint8_t EpN);
     Thread *PThread;
 public:
     void Init();
@@ -65,5 +87,23 @@ public:
 };
 
 extern Usb_t Usb;
+
+
+// ============================== Constants ====================================
+#define USB_REQ_GET_STATUS                  0
+#define USB_REQ_CLEAR_FEATURE               1
+#define USB_REQ_SET_FEATURE                 3
+#define USB_REQ_SET_ADDRESS                 5
+#define USB_REQ_GET_DESCRIPTOR              6
+#define USB_REQ_SET_DESCRIPTOR              7
+#define USB_REQ_GET_CONFIGURATION           8
+#define USB_REQ_SET_CONFIGURATION           9
+#define USB_REQ_GET_INTERFACE               10
+#define USB_REQ_SET_INTERFACE               11
+#define USB_REQ_SYNCH_FRAME                 12
+
+#define USB_FEATURE_ENDPOINT_HALT           0
+#define USB_FEATURE_DEVICE_REMOTE_WAKEUP    1
+#define USB_FEATURE_TEST_MODE               2
 
 #endif /* KL_USB_H_ */
