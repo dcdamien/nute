76daff6101d94a75d9858f9c1404b3a3b8de825f
keltur77 <keltur77@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-07-07 03:37:59 +0000

    exe-dll schema

--- Armlet/OpenSpace/ArmletShell/ShellApi/PlatformDependent.h
+++ Armlet/OpenSpace/ArmletShell/ShellApi/PlatformDependent.h
@@ -6,21 +6,25 @@
 	#define MUTEX_SIZE sizeof(void*)
 	#define FILE_SIZE sizeof(void*)
 
-#ifdef _PC_VISUALIZER_
-	#define __SYSCALL __declspec(dllexport) __cdecl
-	#define __CALLOUT __declspec(dllimport) __cdecl
-	#define __NOCALL __cdecl
-	#define __FUTURE __cdecl
 	#define __CALLBACK __cdecl
+
+#ifdef _PC_VISUALIZER_
+	#define __LOWCALL __declspec(dllimport) __cdecl
+	#define __LOWVAR  __declspec(dllimport)
+	#define __CALLOUT __declspec(dllimport) __cdecl
+#else
+#ifdef _PC_KERNEL_
+	#define __LOWCALL __declspec(dllexport) __cdecl
+	#define __LOWVAR  __declspec(dllexport)
+	#define __SYSCALL __declspec(dllexport) __cdecl
 #else
 #ifdef _ARMLET_
 	#define __SYSCALL __declspec(dllimport) __cdecl
 	#define __CALLOUT __declspec(dllexport) __cdecl
 	#define __NOCALL __cdecl
-	#define __FUTURE __cdecl
-	#define __CALLBACK __cdecl
 #else
 	#error "Platform component unknown"
+#endif
 #endif
 #endif
 
@@ -36,7 +40,6 @@
 	#define __NOCALL
 	#define __CALLOUT
 	#define __SYSCALL
-	#define __FUTURE
 	#define __CALLBACK
 
 #endif
