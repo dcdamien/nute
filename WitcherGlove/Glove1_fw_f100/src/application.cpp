f09c035ae45af1ec7007000fcac98ca11e8ff616
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-07-24 15:54:02 +0000

    Angle-based measurements

--- WitcherGlove/Glove1_fw_f100/src/application.cpp
+++ WitcherGlove/Glove1_fw_f100/src/application.cpp
@@ -17,13 +17,15 @@
 #include "VibroSequences.h"
 #include "vibro.h"
 
+#include <math.h>
+
 App_t App;
 
 int8_t signStatus=-1;
 bool newSign=false;
 
 
-#define TOLERANCE   200
+#define TOLERANCE   40
 #define T_FILTER    5
 
 #define INIT_FORCE  80
@@ -55,8 +57,38 @@ void Process_t::AddAcc(uint8_t ID, int16_t *PAcc) {
 }
 
 bool Process_t::IsSimilar(const int16_t *PSign, const int16_t *PSignMask) {
-    for(uint8_t i=0; i<ACC_VECTOR_SZ; i++)
-        if(Abs(PSign[i] - AccVector[i])*PSignMask[i] > TOLERANCE) return false;
+    double xS, xC, xX; //sign coord vs current coord vs cross.
+    double yS, yC, yX; //cross is called X, because C is already in use
+    double zS, zC, zX;
+    for (uint8_t i=0; i<(ACC_VECTOR_SZ/3); i++){
+        xS=PSign[i*3];
+        yS=PSign[i*3+1];
+        zS=PSign[i*3+2];
+
+        xC=AccVector[i*3];
+        yC=AccVector[i*3+1];
+        zC=AccVector[i*3+2];
+
+        xX=yS*zC-zS*yC;
+        yX=-xS*zC+zS*xC;
+        zX=xS*yC-yS*xC;
+
+        double dotL=double(xS*xC+yS*yC+zS*zC);
+        double crossL=sqrt(double(xX*xX+yX*yX+zX*zX));
+        double signL=sqrt(double(xS*xS+yS*yS+zS*zS));
+        double curL=sqrt(double(xC*xC+yC*yC+zC*zC));
+
+
+        crossL=crossL/(signL*curL); //sin(alpha)=y/d
+        dotL=dotL/(signL*curL); //cos(alpha)=x/d
+
+        //let d be 1
+
+        double angle=(atan2(crossL,dotL)*180/M_PI);
+
+
+        if (int(angle) > TOLERANCE) return false;
+    }
     return true;
 }
 
@@ -107,12 +139,9 @@ static WORKING_AREA(waAppThread, 1024);
 static msg_t AppThread(void *arg) {
     (void)arg;
     chRegSetThreadName("App");
-
     int8_t IsSimilar=-1;
-    int16_t chance=INIT_CHANCE;
-
-
-
+    int8_t OldSim =-1;
+//    int16_t chance=INIT_CHANCE;
 /*
  -1 - idle
  0,1,2 - aard inited/forced/executed        blue
@@ -121,27 +150,15 @@ static msg_t AppThread(void *arg) {
  9,10,11 - irden inited/forced/executed     green
  12,13,14 - kven inited/forced/executed     yellow
  */
-
-
     Vibro.On(100);
     chThdSleepMilliseconds(999);
     Vibro.Off();
 
     TmrReset();
 
-    int8_t OldSim=0;
-
     while(1) {
         chThdSleepMilliseconds(9);
         if(!App.Enabled) continue;  // Do not do anything
-
-        /*YellowLed.On(LED_TOP_BRT);
-        RedLed.On(LED_TOP_BRT);
-        GreenLed.On(LED_TOP_BRT);
-        BlueLed.On(LED_TOP_BRT);
-        UvLed.On(LED_TOP_BRT);*/
-
-//        Uart.Printf("I'm here\n\r");
 
         for(uint8_t i=0; i<ACC_CNT; i++) {
             if(Acc[i].IsOperational) {
@@ -152,9 +169,8 @@ static msg_t AppThread(void *arg) {
                 IsSimilar=-1;
                 for(uint8_t j=0; j<SUBSIGN_CNT; j++) {
                     if(Process.IsSimilar(Subsign[j],SubsignMask[j])) {
-//                        Process.CurrentSubsign = j;
-//                        Uart.Printf("Subs: %u\r", j);
-                        //RedLed.On(54);
+                        Process.CurrentSubsign = j;
+                        Uart.Printf("Subs: %u\r", j);
                         IsSimilar=j;
                     }
                 }
@@ -163,138 +179,158 @@ static msg_t AppThread(void *arg) {
                     Uart.Printf("S: %d\r", IsSimilar);
                     OldSim = IsSimilar;
                 }
-
-                //if(!IsSimilar) RedLed.Off();
-                switch (IsSimilar){
-                    case 0:
-                        resetSign();
-                        Vibro.On(INIT_FORCE);
-                        signStatus=0;
-                        TmrReset();
-                    break;
-                    case 1:
-                        if ((signStatus == 0)||(signStatus == 1)){
-                            signStatus=1;
-                            Vibro.On(CALLED_FORCE);
-                        }else{
-                            if ((signStatus == 3)||(signStatus == 4)){
-                                signStatus=4;
-                                Vibro.On(CALLED_FORCE);
-                            }else{
-                                if ((signStatus == 6)||(signStatus == 7)){
-                                    signStatus=7;
-                                    Vibro.On(CALLED_FORCE);
-                                }else{
-                                    if ((signStatus == 9)||(signStatus == 10)){
-                                        signStatus=10;
-                                        Vibro.On(CALLED_FORCE);
-                                    }else{
-                                        if ((signStatus == 12)||(signStatus == 13)){
-                                            signStatus=13;
-                                            Vibro.On(CALLED_FORCE);
-                                        }else{
-                                            resetSign();
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    break;
-                    case 2:
-                        if ((signStatus == 1)||(signStatus == 2)){
-                            signStatus=2;
-                            BlueLed.On(LED_TOP_BRT);
-                            //chThdSleepMilliseconds(2000);
-                            //resetSign();
-                            chance=INIT_CHANCE;
-                            signExecForce();
-                        }else{
-                            if ((signStatus == 7)||(signStatus == 8)){
-                                signStatus=8;
-                                RedLed.On(LED_TOP_BRT);
-                                //chThdSleepMilliseconds(2000);
-                                //resetSign();
-                                chance=INIT_CHANCE;
-                                signExecForce();
-                            }else{
-                                resetSign();
-                            }
-                        }
-                    break;
-                    case 3:
-                        resetSign();
-                        Vibro.On(INIT_FORCE);
-                        signStatus=3;
-                        TmrReset();
-                    break;
-                    case 4:
-                        if ((signStatus == 4)||(signStatus == 5)){
-                            signStatus=5;
-                            UvLed.On(LED_TOP_BRT);
-                            //chThdSleepMilliseconds(2000);
-                            //resetSign();
-                            chance=INIT_CHANCE;
-                            signExecForce();
-                        }else{
-                            resetSign();
-                        }
-                    break;
-                    case 5:
-                        resetSign();
-                        Vibro.On(INIT_FORCE);
-                        signStatus=6;
-                        TmrReset();
-                    break;
-                    case 6:
-                        resetSign();
-                        Vibro.On(INIT_FORCE);
-                        signStatus=9;
-                        TmrReset();
-                    break;
-                    case 7:
-                        if ((signStatus == 10)||(signStatus == 11)){
-                            signStatus=11;
-                            GreenLed.On(LED_TOP_BRT);
-                            //chThdSleepMilliseconds(2000);
-                            //resetSign();
-                            chance=INIT_CHANCE;
-                            signExecForce();
-                        }else{
-                            if ((signStatus == 13)||(signStatus == 14)){
-                                signStatus=14;
-                                YellowLed.On(LED_TOP_BRT);
-                                //chThdSleepMilliseconds(2000);
-                                //resetSign();
-                                chance=INIT_CHANCE;
-                                signExecForce();
-                            }else{
-                                resetSign();
-                            }
-                        }
-
-                    break;
-                    case 8:
-                        resetSign();
-                        Vibro.On(INIT_FORCE);
-                        signStatus=12;
-                        TmrReset();
-                    break;
-                    default:
-                        if (((signStatus-2)%3==0)){
-                            chance=chance-1;
-                            if (chance<0) resetSign();
-                        }
-                    break;
-                }
-                CheckTimer();
-
-
             }
-            //else Acc[i].Init();
-        }
-        //Process.PrintVect();
-        GreenLed.Off();
-    } // if enabled
+            else Acc[i].Init();
+        } // for i
+    } // while 1
+
+//
+//                IsSimilar=-1;
+//                for(uint8_t j=0; j<SUBSIGN_CNT; j++) {
+//                    if(Process.IsSimilar(Subsign[j],SubsignMask[j])) {
+////                        Process.CurrentSubsign = j;
+////                        Uart.Printf("Subs: %u\r", j);
+//                        //RedLed.On(54);
+//                        IsSimilar=j;
+//                    }
+//                }
+//
+//                if(IsSimilar != OldSim) {
+//                    Uart.Printf("S: %d\r", IsSimilar);
+//                    OldSim = IsSimilar;
+//                }
+//
+//                //if(!IsSimilar) RedLed.Off();
+//                switch (IsSimilar){
+//                    case 0:
+//                        resetSign();
+//                        Vibro.On(INIT_FORCE);
+//                        signStatus=0;
+//                        TmrReset();
+//                    break;
+//                    case 1:
+//                        if ((signStatus == 0)||(signStatus == 1)){
+//                            signStatus=1;
+//                            Vibro.On(CALLED_FORCE);
+//                        }else{
+//                            if ((signStatus == 3)||(signStatus == 4)){
+//                                signStatus=4;
+//                                Vibro.On(CALLED_FORCE);
+//                            }else{
+//                                if ((signStatus == 6)||(signStatus == 7)){
+//                                    signStatus=7;
+//                                    Vibro.On(CALLED_FORCE);
+//                                }else{
+//                                    if ((signStatus == 9)||(signStatus == 10)){
+//                                        signStatus=10;
+//                                        Vibro.On(CALLED_FORCE);
+//                                    }else{
+//                                        if ((signStatus == 12)||(signStatus == 13)){
+//                                            signStatus=13;
+//                                            Vibro.On(CALLED_FORCE);
+//                                        }else{
+//                                            resetSign();
+//                                        }
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    break;
+//                    case 2:
+//                        if ((signStatus == 1)||(signStatus == 2)){
+//                            signStatus=2;
+//                            BlueLed.On(LED_TOP_BRT);
+//                            //chThdSleepMilliseconds(2000);
+//                            //resetSign();
+//                            chance=INIT_CHANCE;
+//                            signExecForce();
+//                        }else{
+//                            if ((signStatus == 7)||(signStatus == 8)){
+//                                signStatus=8;
+//                                RedLed.On(LED_TOP_BRT);
+//                                //chThdSleepMilliseconds(2000);
+//                                //resetSign();
+//                                chance=INIT_CHANCE;
+//                                signExecForce();
+//                            }else{
+//                                resetSign();
+//                            }
+//                        }
+//                    break;
+//                    case 3:
+//                        resetSign();
+//                        Vibro.On(INIT_FORCE);
+//                        signStatus=3;
+//                        TmrReset();
+//                    break;
+//                    case 4:
+//                        if ((signStatus == 4)||(signStatus == 5)){
+//                            signStatus=5;
+//                            UvLed.On(LED_TOP_BRT);
+//                            //chThdSleepMilliseconds(2000);
+//                            //resetSign();
+//                            chance=INIT_CHANCE;
+//                            signExecForce();
+//                        }else{
+//                            resetSign();
+//                        }
+//                    break;
+//                    case 5:
+//                        resetSign();
+//                        Vibro.On(INIT_FORCE);
+//                        signStatus=6;
+//                        TmrReset();
+//                    break;
+//                    case 6:
+//                        resetSign();
+//                        Vibro.On(INIT_FORCE);
+//                        signStatus=9;
+//                        TmrReset();
+//                    break;
+//                    case 7:
+//                        if ((signStatus == 10)||(signStatus == 11)){
+//                            signStatus=11;
+//                            GreenLed.On(LED_TOP_BRT);
+//                            //chThdSleepMilliseconds(2000);
+//                            //resetSign();
+//                            chance=INIT_CHANCE;
+//                            signExecForce();
+//                        }else{
+//                            if ((signStatus == 13)||(signStatus == 14)){
+//                                signStatus=14;
+//                                YellowLed.On(LED_TOP_BRT);
+//                                //chThdSleepMilliseconds(2000);
+//                                //resetSign();
+//                                chance=INIT_CHANCE;
+//                                signExecForce();
+//                            }else{
+//                                resetSign();
+//                            }
+//                        }
+//
+//                    break;
+//                    case 8:
+//                        resetSign();
+//                        Vibro.On(INIT_FORCE);
+//                        signStatus=12;
+//                        TmrReset();
+//                    break;
+//                    default:
+//                        if (((signStatus-2)%3==0)){
+//                            chance=chance-1;
+//                            if (chance<0) resetSign();
+//                        }
+//                    break;
+//                }
+//                CheckTimer();
+//
+//
+//            }
+//            //else Acc[i].Init();
+//        }
+//        //Process.PrintVect();
+//        GreenLed.Off();
+//    } // if enabled
     return 0;
 }
 
