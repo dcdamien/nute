6e741c608696c98da19f29dcd115ddfc895a365a
mourhoon@gmail.com <mourhoon@gmail.com@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-08-28 22:40:43 +0000

    ShipStatus added. Including RanmaSubsystemStatus.cs

---
+++ trunk/Armlet/Armlet3/BastiliaGate_sw/Server/HonorLogic/ShipStatus/BigShip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HonorLogic.ShipStatus
+{
+    public class BigShip: IShip
+    {
+        private ShipType _shipType = ShipType.BigShip;
+        public ShipType GetShipType()
+        {
+            return _shipType;
+        }
+
+        public bool SetSubsystemStatus(int subsystemNum, RanmaSubsystemStatus ranmaStatus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<RanmaSubsystemStatus> GetAllSubsystemsStatus()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ trunk/Armlet/Armlet3/BastiliaGate_sw/Server/HonorLogic/ShipStatus/ShipType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HonorLogic.ShipStatus
+{
+    public enum ShipType
+    {
+        // Number of available ship subsystems are hard-coded here, in enum values
+        BigShip = 8,
+        LakShip = 4
+    }
+}
