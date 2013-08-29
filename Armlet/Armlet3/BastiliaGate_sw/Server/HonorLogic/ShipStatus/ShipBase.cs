28a7eab2dbed24d6986845116e9ab8deb1bc2641
leo@bastilia.ru <leo@bastilia.ru@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-08-29 22:19:35 +0000

    Названия комнат

--- Armlet/Armlet3/BastiliaGate_sw/Server/HonorLogic/ShipStatus/ShipBase.cs
+++ Armlet/Armlet3/BastiliaGate_sw/Server/HonorLogic/ShipStatus/ShipBase.cs
@@ -3,22 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using HonorInterfaces;
+using HonorLogic.Storage;
 
 namespace HonorLogic.ShipStatus
 {
     public abstract class ShipBase : IShip
     {
+        public List<RoomInfo> Rooms { get; set; }
         public abstract int BoardCount { get; }
         public abstract bool SetSubsystemStatus(ShipSubsystemStatus subsystemStatus);
         public abstract List<ShipSubsystemStatus> GetAllSubsystemsStatus();
-        public  List<byte> ShipRoomsIDs { get; set; }
+
+        public List<byte> ShipRoomsIDs
+        {
+            get
+            {
+                return Rooms.Select(r => r.Id).ToList();
+            } 
+        }
+
         public  Guid ShipGuid { get; set; }
         public  int[] PhysicalGateID { get; set; }
         public  string Name { get; set; }
         public IGlobalModel Model { get; set; }
         public string GetRoomName(byte roomsID)
         {
-            return "Отсек " + roomsID;
+            return FndRoomName(roomsID) ?? 
+             "Отсек " + roomsID;
+        }
+
+        private string FndRoomName(byte roomsID)
+        {
+            return (from roomInfo in Rooms where roomInfo.Id == roomsID select roomInfo.Name).SingleOrDefault();
         }
 
         public event Action<ShipSubsystemStatus> SubsystemUpdated;
