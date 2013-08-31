7c5d5b55ea6ca2db50c992e374a3babcd0d8deb8
leo@bastilia.ru <leo@bastilia.ru@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-08-31 12:32:01 +0000

    Удалил весь дублирующий код из {Lak/Big}Ship
    Теперь они отличаются ТОЛЬКО количеством плат и подсистем.

--- Armlet/Armlet3/BastiliaGate_sw/Server/HonorLogic/ShipStatus/BigShip.cs
+++ Armlet/Armlet3/BastiliaGate_sw/Server/HonorLogic/ShipStatus/BigShip.cs
@@ -6,22 +6,6 @@ namespace HonorLogic.ShipStatus
 {
     public class BigShip: ShipBase
     {
-        protected override void SyncSubsystem(int i)
-        {
-            Debug.Assert(PlatesCount == 2);
-
-            var firstPlate = RanmaPlates[0].plateStatusList.First(a => a.SubSystemNum == i);
-            var secondPlate = RanmaPlates[1].plateStatusList.First(a => a.SubSystemNum == i);
-            if (((firstPlate.Severity == RanmaRepairSeverity.Ready) &&
-                 (secondPlate.Severity != RanmaRepairSeverity.Ready)) ||
-                ((firstPlate.Severity != RanmaRepairSeverity.Ready) &&
-                 (secondPlate.Severity == RanmaRepairSeverity.Ready)))
-            {
-                RanmaPlates[0].SetPlateSubsystemInfo(i, RanmaRepairSeverity.Ready, new RanmaSubsystemTable(0));
-                RanmaPlates[1].SetPlateSubsystemInfo(i, RanmaRepairSeverity.Ready, new RanmaSubsystemTable(0));
-            }
-        }
-
         public override int SubsystemsCount
         {
             get { return 8; }
