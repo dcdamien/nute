ee80ab308d06f566602795015ccc4512ab78df2e
leo@bastilia.ru <leo@bastilia.ru@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2013-09-03 22:07:20 +0000

    Пересылать на браслет тип взрыва

---
+++ Armlet/Armlet3/BastiliaGate_sw/Server/PillInterfaces/HitType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HonorInterfaces
+{
+    public enum HitType
+    {
+        Random = 0,
+        Blow = 1,
+        Thermal = 2,
+        Radiation =3 
+    }
+}
--- Armlet/Armlet3/BastiliaGate_sw/Server/PillInterfaces/IGlobalModel.cs
+++ Armlet/Armlet3/BastiliaGate_sw/Server/PillInterfaces/IGlobalModel.cs
@@ -13,7 +13,7 @@ namespace HonorInterfaces
         void SendSetName(string text, IArmletInfo armletId);
         void SendShowMessage(IArmletInfo armlet, string text);
         void SetSetPlayerRegen(IArmletInfo armlet, byte regen);
-        void SendRoomHit(byte roomId, byte hitChance);
+        void SendRoomHit(byte roomId, byte hitChance, HitType hitType = HitType.Random);
         void UpdateAllNames();
         IEnumerable<IShip> GetShips();
         IEnumerable<byte> RoomsWithPeople { get; }
