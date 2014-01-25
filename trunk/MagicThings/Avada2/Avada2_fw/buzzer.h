fd911a65cc3ab165fab87ef796a3f30127c5e7e5
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2014-01-25 16:14:39 +0000

    Flash and Buzz completed. + Resistor to eliminate power-on flash

--- trunk/MagicThings/Avada2/Avada2_fw/buzzer.h
+++ trunk/MagicThings/Avada2/Avada2_fw/buzzer.h
@@ -12,8 +12,6 @@
 
 #define BUZZER_TIM_N        1 // TIM1
 #define BUZZER_TIM_CH       1
-#define BUZZER_TIM_TOP      10
-#define BUZZER_TIM_VALUE    1
 #define BUZZER_GPIO         GPIOA
 #define BUZZER_PIN          8
 
@@ -22,27 +20,21 @@
 #define PERIOD_MAX  3600    // Lowest frequency
 #define PERIOD_MIN  315     // Highest frequency
 // Sound volume
-#define VOLUME      7
+#define VOLUME      4
 
 class Buzz_t {
 private:
-    uint16_t IFreq, IPeriod, IVolume;
     VirtualTimer ITmr;
     PwmPin_t IPin;
-    uint32_t Delay() {
-        if(IPeriod > 900) return 1;
-        else if(IPeriod > 600) return 4;
-        else return 9;
-    }
 public:
-    void Init() {
-        IPin.Init(BUZZER_GPIO, BUZZER_PIN, BUZZER_TIM_N, BUZZER_TIM_CH, BUZZER_TIM_TOP);
-    }
+    uint16_t IPeriod;
+    void Init() { IPin.Init(BUZZER_GPIO, BUZZER_PIN, BUZZER_TIM_N, BUZZER_TIM_CH, PERIOD_MAX); }
     void BuzzUp();
     void Off() {
-        // TODO: reset tmr
+        chVTReset(&ITmr);
         IPin.Off();
     }
+    bool IsOnTop() { return (IPeriod <= PERIOD_MIN); }
     // Inner use
     void ITmrCallback();
 };
