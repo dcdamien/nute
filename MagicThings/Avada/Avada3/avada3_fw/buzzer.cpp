e8462b278d54b4df07d44bcbce8342c189547267
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2014-04-10 12:17:18 +0000

    Avada3 firmware release candidate

---
+++ MagicThings/Avada/Avada3/avada3_fw/buzzer.cpp
@@ -0,0 +1,33 @@
+/*
+ * buzzer.cpp
+ *
+ *  Created on: 25 џэт. 2014 у.
+ *      Author: Kreyl
+ */
+
+#include "buzzer.h"
+
+Buzz_t Buzzer;
+
+void BuzzTmrCallback(void *p) { Buzzer.ITmrCallback(); }
+
+void Buzz_t::ITmrCallback() {
+    if     (IPeriod > 2700) IPeriod -= 18;
+    else if(IPeriod > 1800) IPeriod -= 7;
+    else if(IPeriod > 1350) IPeriod -= 4;
+    else if(IPeriod > 900 ) IPeriod -= 3;
+    else if(IPeriod > 603 ) IPeriod -= 2;
+    else                    IPeriod -= 1;
+    IPin.SetTop(IPeriod);
+    if(IPeriod >= PERIOD_MIN) {
+        chVTSetI(&ITmr, MS2ST(BUZZ_DELAY_MS), (vtfunc_t)&BuzzTmrCallback, nullptr);
+    }
+}
+
+void Buzz_t::BuzzUp() {
+    IPeriod = PERIOD_MAX;
+    IPin.SetTop(IPeriod);
+    IPin.Set(VOLUME_NORMAL);
+    chVTSetI(&ITmr, MS2ST(BUZZ_DELAY_MS), (vtfunc_t)&BuzzTmrCallback, nullptr);
+}
+
