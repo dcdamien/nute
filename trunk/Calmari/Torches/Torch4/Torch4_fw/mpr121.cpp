8058ba88260d5e164e4ba49e151e7c99f2ba8cd8
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2014-09-24 22:20:56 +0000

    * MPR touches detected successfully

--- trunk/Calmari/Torches/Torch4/Torch4_fw/mpr121.cpp
+++ trunk/Calmari/Torches/Torch4/Torch4_fw/mpr121.cpp
@@ -8,24 +8,21 @@
 #include "mpr121.h"
 #include "cmd_uart.h"
 
+i2c_t i2c;
+mpr121_t Touch;
+
 #if 1 // ==== Constants ====
+// Electrode configuration:
+// Baseline tracking by default, Eleprox disabled, elements 0...N enabled
+#define ELECTRODE_CFG           (0x00 | 0x00 | MPR_USED_SNS_CNT)
+
 /* The status bits in Status Register 0x00 and 0x01 will only take place after
  * the number of consecutive touch or release detection meets the debounce number setting. */
 //#define DEBOUNCE_REG_VALUE  0x11
 
-template <uint8_t Sz>
-struct Settings_t {
-    uint8_t Addr;
-    uint8_t Arr[Sz];
-    uint8_t Write() const {
-        for(uint8_t i=Addr; i<Addr+Sz; i++) Uart.Printf("\r%u", Arr[i]);
-        return OK;
-    }
-} __attribute__((__packed__));
-
 // ==== Filtering settings ====
-const Settings_t<8> Filtering = {
-        0x2B,
+const uint8_t Filtering[] = {
+        0x2B,                   // Reg addr
         0x01, 0x01, 0x00, 0x00, // Rising:  MHD, NHD, NCL, FDL
         0x01, 0x01, 0xFF, 0x02  // Falling: MHD, NHD, NCL, FDL
 };
@@ -36,8 +33,8 @@ const Settings_t<8> Filtering = {
  * and set the touch at 80% and release at 70% of delta for good performance */
 #define TOUCH_THRESHOLD     15
 #define RELEASE_THRESHOLD   10
-const Settings_t<(MPR_USED_SNS_CNT * 2)> Thresholds = {
-        0x41,
+const uint8_t Thresholds[] = {
+        0x41, // Reg addr
         TOUCH_THRESHOLD, RELEASE_THRESHOLD,
         TOUCH_THRESHOLD, RELEASE_THRESHOLD,
         TOUCH_THRESHOLD, RELEASE_THRESHOLD,
@@ -45,21 +42,12 @@ const Settings_t<(MPR_USED_SNS_CNT * 2)> Thresholds = {
         TOUCH_THRESHOLD, RELEASE_THRESHOLD,
 };
 
-// ==== Filter cfg ====
-const Settings_t<1> FilterCfg = {
-        0x5D,
-        0x04
-};
-
-// ==== Electrode cfg ==== Write last!
-const Settings_t<1> ElectrodeCfg = {
-        0x5E,
-        ELECTRODE_CFG
-};
+// ==== Filter cfg (0x5D) ==== except ESI
+#define FILTER_CFG  0x00    // Electrode charging dis, 4 samples (default)
 
 // ==== Auto-configuration ====
-const Settings_t<5> AutoCfg = {
-        0x7B,
+const uint8_t AutoCfg[] = {
+        0x7B, // Reg addr
         0x0B, // Reg0; AutoCfg en, AutoRecfg en, Baseline auto en. Do as AN3889 p.9 says and do not ask.
         0x00, // Reg1; Both CDTx and CDCx will be searched and set by autoconfiguration and/or autoreconfiguration; no failure IRQs.
         ((256 * (MPR_VDD_MV - 700))      / MPR_VDD_MV),         // USL; AN3889 p.7
@@ -67,21 +55,93 @@ const Settings_t<5> AutoCfg = {
         ((256 * (MPR_VDD_MV - 700) * 90) / (100 * MPR_VDD_MV))  // TargetLevel; AN3889 p.8
 };
 
-// ==== Reset ====
-const Settings_t<1> ResetReg = {
-        0x80,
-        0x63    // Value to write to reset reg to reset device
+// ==== Baseline values ==== experimental obtained data
+const uint8_t Baseline[] = {
+        0x1E,   // reg addr
+        0xB4, 0xB4, 0xB4, 0xB4, 0xB4
 };
-
 #endif
 
-mpr121_t Touch;
+#if 1 // ==== Thread ====
+static WORKING_AREA(waMprThread, 256);
+__attribute__ ((__noreturn__))
+static void MprThread(void *arg) {
+    chRegSetThreadName("MprThd");
+    while(true) Touch.ITask();
+}
+
+void mpr121_t::ITask() {
+    chThdSleepMilliseconds(99);
+    ReadOutput();
+    Uart.Printf("\rTS: %04X", Output.TouchStatus);
+    Uart.Printf("   oo: %04X   ", Output.OorStatus);
+    for(uint8_t i=0; i<MPR_USED_SNS_CNT; i++) Uart.Printf("%04X ", Output.EleData[i]);
+    Uart.Printf("   ");
+    for(uint8_t i=0; i<MPR_USED_SNS_CNT; i++) Uart.Printf("%02X ", Output.Baseline[i]);
+//    Uart.Printf("\r");
+    Uart.Printf("   ");
+    if(Output.TouchStatus & (1<<2)) Uart.Printf("V "); else Uart.Printf("_ ");
+    if(Output.TouchStatus & (1<<3)) Uart.Printf("V "); else Uart.Printf("_ ");
+    if(Output.TouchStatus & (1<<4)) Uart.Printf("V "); else Uart.Printf("_ ");
+    if(Output.TouchStatus & (1<<1)) Uart.Printf("V "); else Uart.Printf("_ ");
+    if(Output.TouchStatus & (1<<0)) Uart.Printf("V "); else Uart.Printf("_ ");
+
+}
+#endif
 
 void mpr121_t::Init() {
-    Filtering.Write();
-    Thresholds.Write();
-    FilterCfg.Write();
-    AutoCfg.Write();
+    i2c.Init(MPR_I2C, MPR_I2C_GPIO, MPR_SCL_PIN, MPR_SDA_PIN, MPR_I2C_BITRATE_HZ, MPR_DMATX, MPR_DMARX);
+    Reset();
+    chThdSleepMilliseconds(1);
+    Write((void*)Filtering, countof(Filtering));
+    Write((void*)Thresholds, countof(Thresholds));
+    Write((void*)AutoCfg, countof(AutoCfg));
+    Write((void*)Baseline, countof(Baseline));
+    SetSampleInterval(MPR_SAMPLE_INTRVL_MS);
 
-    ElectrodeCfg.Write();
+//    Irq.Setup(MPR_IRQ_GPIO, MPR_IRQ_PIN, ttFalling);
+    PThd = chThdCreateStatic(waMprThread, sizeof(waMprThread), NORMALPRIO, (tfunc_t)MprThread, NULL);
+    On();
+}
+
+void mpr121_t::On()  { Write(0x5E, ELECTRODE_CFG); }
+void mpr121_t::Off() { Write(0x5E, 0); }
+
+void mpr121_t::SetSampleInterval(uint8_t SampleInterval) {
+    uint8_t Esi = 0;
+    switch(SampleInterval) {
+        case 1:   Esi = 0; break;
+        case 2:   Esi = 1; break;
+        case 4:   Esi = 2; break;
+        case 8:   Esi = 3; break;
+        case 16:  Esi = 4; break;
+        case 32:  Esi = 5; break;
+        case 64:  Esi = 6; break;
+        case 128: Esi = 7; break;
+        default:
+            Uart.Printf("\rMpr Wrong Interval");
+            return;
+            break;
+    } // switch
+    Write(0x5D, FILTER_CFG | Esi);
+}
+
+void mpr121_t::Write(void *PRegData, uint8_t Length) {
+    uint8_t r = i2c.CmdWriteWrite(MPR_I2C_ADDR, PRegData, Length, nullptr, 0);
+    if(r != OK) Uart.Printf("\rmprW %A Fail", PRegData, Length, ' ');
+    else Uart.Printf("\rmprW %A Ok", PRegData, Length, ' ');
+}
+void mpr121_t::Write(uint8_t RegAddr, uint8_t AData) {
+    uint8_t Buf[2];
+    Buf[0] = RegAddr;
+    Buf[1] = AData;
+    uint8_t r = i2c.CmdWriteWrite(MPR_I2C_ADDR, Buf, 2, nullptr, 0);
+    if(r != OK) Uart.Printf("\rmprW %A Fail", Buf, 2, ' ');
+    else Uart.Printf("\rmprW %A Ok", Buf, 2, ' ');
+}
+
+void mpr121_t::Read(uint8_t RegAddr, void *PData, uint8_t Length) {
+    uint8_t r = i2c.CmdWriteRead(MPR_I2C_ADDR, &RegAddr, 1, PData, Length);
+    if(r != OK) Uart.Printf("\rmprR %X Fail", RegAddr);
+//    else Uart.Printf("\rmprR %A", PData, Length, ' ');
 }
--- trunk/Calmari/Torches/Torch4/Torch4_fw/mpr121.h
+++ trunk/Calmari/Torches/Torch4/Torch4_fw/mpr121.h
@@ -9,18 +9,34 @@
 #define MPR121_H_
 
 #include "ch.h"
+#include "hal.h"
+#if defined STM32L1XX_HD || defined STM32L1XX_MD
+#include "kl_lib_L15x.h"
+#else
 #include "kl_lib_f072.h"
+#endif
 
+// I2C
 #define MPR_I2C                 I2C1
 #define MPR_I2C_GPIO            GPIOB
+#define MPR_SCL_PIN             8
+#define MPR_SDA_PIN             9
+#define MPR_I2C_BITRATE_HZ      100000
+#define MPR_DMATX               STM32_DMA1_STREAM6
+#define MPR_DMARX               STM32_DMA1_STREAM7
 
+// IRQ
+#define MPR_IRQ_GPIO            GPIOB
+#define MPR_IRQ_PIN             10
+
+// I2C address
 #define MPR_I2C_ADDR            0x5A    // Addr pin connected to VSS
 
 #define MPR_USED_SNS_CNT        5       // ELE0, ELE1, ...
+#define MPR_SAMPLE_INTRVL_MS    16      // 16 by default; 1,2,4,...16,32,...128
+#define MPR_SAMPLE_INTRVL_MS_LP 128     // Sample interval for low power mode
 #define MPR_VDD_MV              3300    // Voltage required for autocfg settings
-// Electrode configuration:
-// Baseline tracking by default, Eleprox disabled, elements 0...4 enabled
-#define ELECTRODE_CFG           0b00000101  //
+
 
 #if 1 // ==== Registers and constants ====
 #define MPR_TOTAL_SNS_CNT               13  // ELE0, ELE1, ..., ELEPROX
@@ -38,21 +54,27 @@ struct mpr121Output_t {
     uint8_t Baseline[MPR_TOTAL_SNS_CNT];
 } __attribute__((__packed__));
 
-struct mpr121Gpio_t {
-    uint8_t Ctrl0, Ctrl1, Data, Direction, Enable, Set, Clear, Toggle;
-} __attribute__((__packed__));
-
 class mpr121_t {
 private:
     mpr121Output_t Output;
+    PinIrq_t Irq;
+    Thread *PThd;
     // High-level cmds
     // Middle-level cmds
-    uint8_t ClearOvercurrentBit() { return Write(MPR_REG_TOUCH_STATUS1, 0x80); }    // Write 1 to OVCF bit
+    void ReadOutput() { Read(MPR_REG_TOUCH_STATUS0, &Output, sizeof(mpr121Output_t)); }
+    void ClearOvercurrentBit() { Write(MPR_REG_TOUCH_STATUS1, 0x80); }    // Write 1 to OVCF bit
     // Low-level i2c communication
-    uint8_t Write(uint8_t RegAddr, uint8_t AData) { return OK; }  // Single byte at a time only
-    uint8_t Read(uint8_t RegAddr, void *PData, uint8_t Length);
+    void Write(void *PRegData, uint8_t Length); // PRegdata contains reg addr first
+    void Write(uint8_t RegAddr, uint8_t AData);
+    void Read(uint8_t RegAddr, void *PData, uint8_t Length);
+    void SetSampleInterval(uint8_t SampleInterval);
+    void Reset() { Write(0x80, 0x63); }
 public:
     void Init();
+    void On();
+    void Off();
+    // Inner use
+    void ITask();
 };
 
 extern mpr121_t Touch;
