7da4c829f14f03260054ddb5f00d6717a6c3d1e1
laellin <laellin@3623c4fa-fa74-b9b9-717e-9badfcd41e35>
2015-01-18 23:41:17 +0000

    + SndList. It works.  Something bad with mp3 playback.

--- Lock/LockNFC3/LockNFC3_f205_fw/main.h
+++ Lock/LockNFC3/LockNFC3_f205_fw/main.h
@@ -14,27 +14,8 @@
 #include "led_rgb.h"
 #include "Sequences.h"
 
-// ==== Sound files ====
-#define SND_COUNT_MAX   100
-
-struct Snd_t {
-     char Filename[MAX_NAME_LEN];
-     uint32_t ProbBottom, ProbTop;
-};
-
-struct SndList_t {
-    Snd_t Phrases[SND_COUNT_MAX];
-    int32_t Count;
-    int32_t ProbSumm;
-};
-
 #define STATE_TIMEOUT       18000   // ms; switch to waiting state
 #define DOOR_CLOSE_TIMEOUT  9999    // ms
-// Colors in RGB
-#define DOOR_CLOSED_COLOR   {4, 0, 0}
-#define DOOR_OPEN_COLOR     {45, 0, 45}
-#define DOOR_CLOSED_BLINK   {45, 0, 0}
-#define DOOR_OPEN_BLINK     {135, 0, 135}
 
 enum DoorState_t {dsClosed, dsOpen};
 enum AppState_t  {asIdle, asAddingAcc, asRemovingAcc, asAddingMaster, asRemovingMaster};
